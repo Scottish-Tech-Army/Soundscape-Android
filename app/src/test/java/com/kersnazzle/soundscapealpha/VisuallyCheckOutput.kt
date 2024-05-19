50b859172dd7738191857abba52ecf41317275aa
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-05-19 09:46:48 +0100

    Test functions for some visual checks of the GeoJSON output

--- app/src/test/java/com/kersnazzle/soundscapealpha/VisuallyCheckOutput.kt
+++ app/src/test/java/com/kersnazzle/soundscapealpha/VisuallyCheckOutput.kt
@@ -8,6 +8,7 @@ import com.kersnazzle.soundscapealpha.utils.circleToPolygon
 import com.kersnazzle.soundscapealpha.utils.cleanTileGeoJSON
 import com.kersnazzle.soundscapealpha.utils.getBoundingBoxCorners
 import com.kersnazzle.soundscapealpha.utils.getCenterOfBoundingBox
+import com.kersnazzle.soundscapealpha.utils.getIntersectionsFeatureCollectionFromTileFeatureCollection
 import com.kersnazzle.soundscapealpha.utils.getPolygonOfBoundingBox
 import com.kersnazzle.soundscapealpha.utils.getRoadsFeatureCollectionFromTileFeatureCollection
 import com.kersnazzle.soundscapealpha.utils.getTilesForRegion
@@ -152,6 +153,31 @@ class VisuallyCheckOutput {
         val roads = moshi.adapter(FeatureCollection::class.java).toJson(testRoadsCollection)
         // copy and paste into GeoJSON.io
         println(roads)
+    }
+
+    @Test
+    fun intersectionsFeatureCollection(){
+        val moshi = GeoMoshi.registerAdapters(Moshi.Builder()).build()
+        // convert coordinates to tile
+        val tileXY = getXYTile(51.43860066718254, -2.69439697265625, 16 )
+        // Get the data for the entire tile
+        val entireFeatureCollectionTest = moshi.adapter(FeatureCollection::class.java)
+            .fromJson(GeoJsonDataReal.featureCollectionJsonRealSoundscapeGeoJson)
+        // clean it
+        val cleanTileFeatureCollection = cleanTileGeoJSON(
+            tileXY.first,
+            tileXY.second,
+            16.0,
+            moshi.adapter(FeatureCollection::class.java).toJson(entireFeatureCollectionTest)
+        )
+        // get the Intersections Feature Collection.
+        val testIntersectionsCollection = getIntersectionsFeatureCollectionFromTileFeatureCollection(
+            moshi.adapter(FeatureCollection::class.java)
+                .fromJson(cleanTileFeatureCollection)!!
+        )
+        val intersections = moshi.adapter(FeatureCollection::class.java).toJson(testIntersectionsCollection)
+        // copy and paste into GeoJSON.io
+        println(intersections)
     }
 
 
