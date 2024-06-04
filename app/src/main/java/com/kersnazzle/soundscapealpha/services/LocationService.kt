ff83193a6a1f5bb783cd986fed1aec75c3686374
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-06-04 19:28:09 +0100

    letting the user know we are running a foreground service

--- app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
+++ app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
@@ -1,15 +1,23 @@
 package com.kersnazzle.soundscapealpha.services
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.kersnazzle.soundscapealpha.R
 
 // Attempting to wrap the FusedLocation API in a foreground service
 class LocationService: Service() {
@@ -35,8 +43,54 @@ class LocationService: Service() {
         super.onStartCommand(intent, flags, startId)
         //Have we started?
         Log.d("${LocationService::class.simpleName}", "onStartCommand")
+        // Yarp. Now create a foreground service
+        startForeground()
 
         return START_STICKY
+    }
+
+    private fun startForeground() {
+        Log.d("--- ${LocationService::class.simpleName}", "Start Foreground")
+        // Notify user that we are running a foreground service so they know we aren't
+        // doing anything naughty other than spanking their battery
+
+        ServiceCompat.startForeground(
+            this,
+            Companion.NOTIFICATION_ID,
+            getNotification(),
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION
+        )
+    }
+
+    private fun getNotification(): Notification {
+        createServiceNotificationChannel()
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.notification_text))
+            // some sort of Soundscape icon thingy here
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setOngoing(true)
+
+        return builder.build()
+    }
+
+    private fun createServiceNotificationChannel() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        // erm, not sure what this should be but not 0 apparently
+        private const val NOTIFICATION_ID = 1000000
+        private const val CHANNEL_ID = "channel_01"
+        private const val NOTIFICATION_CHANNEL_NAME = "SoundscapeAlpha"
+
     }
 
 
