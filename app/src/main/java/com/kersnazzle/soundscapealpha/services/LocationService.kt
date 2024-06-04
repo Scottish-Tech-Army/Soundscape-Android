32df05a60c085ffc1686ff772a1e54f144694aa3
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-06-04 18:20:49 +0100

    Setup Binder so clients can access the service

--- app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
+++ app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
@@ -3,11 +3,20 @@ package com.kersnazzle.soundscapealpha.services
 import android.Manifest
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
 // Attempting to wrap the FusedLocation API in a foreground service
 class LocationService: Service() {
-    override fun onBind(p0: Intent?): IBinder? {
+    // Binder object for local communication with client activities
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        // reference for the clients of this service
+        fun getService(): LocationService = this@LocationService
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
 }
