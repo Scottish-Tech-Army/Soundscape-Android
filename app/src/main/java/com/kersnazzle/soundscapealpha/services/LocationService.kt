db6ea03c5121c2a3f9601d550dbcf76f98b91bd8
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-06-04 18:58:06 +0100

    override onStartCommand to check this is creating the worlds most useless service

--- app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
+++ app/src/main/java/com/kersnazzle/soundscapealpha/services/LocationService.kt
@@ -5,9 +5,18 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 // Attempting to wrap the FusedLocation API in a foreground service
 class LocationService: Service() {
+    // use FusedLocation API
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
     // Binder object for local communication with client activities
     private val binder = LocalBinder()
 
@@ -21,4 +30,14 @@ class LocationService: Service() {
     override fun onBind(intent: Intent?): IBinder {
         return binder
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        //Have we started?
+        Log.d("${LocationService::class.simpleName}", "onStartCommand")
+
+        return START_STICKY
+    }
+
+
 }
