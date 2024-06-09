ddc74eed084b553fa12917fd45b9ffce3122f414
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-06-09 08:45:10 +0100

    Foreground service

--- app/src/main/java/com/kersnazzle/soundscapealpha/ui/theme/Theme.kt
+++ app/src/main/java/com/kersnazzle/soundscapealpha/ui/theme/Theme.kt
@@ -38,6 +38,38 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
+fun ForegroundServiceTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+@Composable
 fun SoundscapeAlphaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
