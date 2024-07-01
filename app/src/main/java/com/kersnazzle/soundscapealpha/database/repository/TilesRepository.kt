0ebfc59c943bebdc4097ab762b98a3a4b6deddfd
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-07-01 07:44:37 +0100

    prototype Realm DB - needs a lot more fiddling

---
+++ app/src/main/java/com/kersnazzle/soundscapealpha/database/repository/TilesRepository.kt
@@ -0,0 +1,25 @@
+package com.kersnazzle.soundscapealpha.database.repository
+
+import com.kersnazzle.soundscapealpha.database.local.dao.TilesDao
+import com.kersnazzle.soundscapealpha.database.local.model.TileData
+import io.realm.kotlin.notifications.ResultsChange
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+class TilesRepository(val tilesDao: TilesDao) {
+
+    suspend fun insertTile(tile: TileData) = withContext(Dispatchers.IO) {
+        tilesDao.insertTile(tile)
+    }
+
+    fun getAllTiles(): Flow<ResultsChange<TileData>> = tilesDao.getAllTiles()
+
+    suspend fun deleteTile(quadkey: String) = withContext(Dispatchers.IO) {
+        tilesDao.deleteTiles(quadkey)
+    }
+
+    suspend fun updateTile(tile: TileData) = withContext(Dispatchers.IO) {
+        tilesDao.updateTile(tile)
+    }
+}
