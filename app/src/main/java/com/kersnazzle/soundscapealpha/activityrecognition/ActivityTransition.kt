a80ee5704e90e2d9a12fc40755a2846fc4841b8e
AsquaredWsquared <76879959+AsquaredWsquared@users.noreply.github.com>
2024-07-09 18:39:26 +0100

    Start and Stop functions for ActivityTransition tracking

--- app/src/main/java/com/kersnazzle/soundscapealpha/activityrecognition/ActivityTransition.kt
+++ app/src/main/java/com/kersnazzle/soundscapealpha/activityrecognition/ActivityTransition.kt
@@ -1,8 +1,14 @@
 package com.kersnazzle.soundscapealpha.activityrecognition
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.ActivityRecognition
 import com.google.android.gms.location.ActivityTransition
 import com.google.android.gms.location.ActivityTransitionRequest
 import com.google.android.gms.location.DetectedActivity
@@ -24,7 +30,11 @@ class ActivityTransition(val context: Context) {
 
     private var pendingIntent: PendingIntent? = null
 
-    fun startVehicleActivityTracking(){
+    @SuppressWarnings("MissingPermission")
+    fun startVehicleActivityTracking(
+        onSuccess: () -> Unit = { },
+        onFailure: (String) -> Unit = { }
+    ){
 
         pendingIntent = PendingIntent.getBroadcast(
             context,
@@ -35,12 +45,50 @@ class ActivityTransition(val context: Context) {
         )
         val request = ActivityTransitionRequest(getTransitions())
 
+        if (permissionGranted()) {
+            pendingIntent?.let { trackingIntent ->
+                ActivityRecognition.getClient(context)
+                    .requestActivityTransitionUpdates(request, trackingIntent)
+                    .addOnSuccessListener {
+                        Log.d(TAG,"Activity recognition started")
+                        onSuccess()
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.d(TAG, "Activity recognition could not be started: ${exception.message}")
+                        onFailure("Activity recognition could not be started: ${exception.message}")
+                    }
+            }
+        }
+
     }
 
+    @SuppressWarnings("MissingPermission")
     fun stopVehicleActivityTracking(){
+        if (permissionGranted()) {
+            pendingIntent?.let { trackingIntent ->
+                ActivityRecognition.getClient(context).removeActivityTransitionUpdates(trackingIntent)
+                    .addOnSuccessListener {
+                        trackingIntent.cancel()
+                        Log.d(TAG, "Activity recognition canceled")
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.e(TAG, "Activity recognition could not be canceled: ${exception.message}")
+                    }
+            }
+        }
 
     }
 
+    private fun permissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACTIVITY_RECOGNITION
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
 
     // TODO Remove the other DetectedActivity once I've figured out how to do this as I'm only
     //  interested in the DetectedActivity.IN_VEHICLE
@@ -87,4 +135,8 @@ class ActivityTransition(val context: Context) {
             .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
             .build(),
     )
+    companion object {
+        private const val TAG = "ActivityTransition"
+    }
 }
+
