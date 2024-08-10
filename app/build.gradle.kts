a93a33c903340701ba42bccbbcb864dca6210ffa
davecraig <davecraig@unbalancedaudio.com>
2024-08-10 12:36:11 +0100

    Extend GpxTest to have multiple routes in database
    Rather than test with one route at a time, we now import all of the GPX
    into the database and then test each one separately.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,7 +113,7 @@ dependencies {
     testImplementation ("org.jetbrains.kotlin:kotlin-test-junit:1.9.10")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
