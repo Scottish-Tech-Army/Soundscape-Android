d29ab0f912a48281a6aee3b42b2b539768d3f979
Dave Craig <davecraig@unbalancedaudio.com>
2024-08-17 10:19:17 +0100

    Update to screenshot test approach
    This change adds a number of different configurations for which the
    Preview is run:
    
     * Locales - all of the languages that we support
     * Devices - tablet, small phone and medium phone
     * Fonts - smaller and larger
    
    Currently this results in 119 files. The names change each time the
    update builds is run. It doesn't make sense to pollute our main
    git branch with these, so I've added them to a .gitignore.
    For github actions, the approach I'll likely take is:
    
     * First new action which runs the update screenshot build and then stores
       the resulting screenshots in a reference artifact for the repo
     * Second action runs the screenshot validation against the retrieved
       reference artifact. Ideall it would then publish the resuling
       org.scottishtecharmy.soundscape.PreviewTest.html to the GitHub pages
       site.
     * Third action runs the update screenshot build but discards the results
    
    The last of these is the most useful in that it would hopfully be useful
    for checking whenever Previews get broken which otherwise go unnoticed in
    the build. The first action could be run manually to get a quick overview of
    the current state of the UI. The second might be more useful once we've got
    a more stable application.
    
    The main problem with not storing the reference images in the repo is that
    rewinding to an older commit won't rewind the reference and so building on a
    branch would get confusing. Needs some more thinking about.

--- app/src/screenshotTest/kotlin/org/scottishtecharmy/soundscape/PreviewTest.kt
+++ app/src/screenshotTest/kotlin/org/scottishtecharmy/soundscape/PreviewTest.kt
@@ -1,6 +1,7 @@
 package org.scottishtecharmy.soundscape
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import org.scottishtecharmy.soundscape.screens.Home
 import org.scottishtecharmy.soundscape.screens.onboarding.Finish
@@ -11,8 +12,36 @@ import org.scottishtecharmy.soundscape.screens.onboarding.ListeningPreview
 import org.scottishtecharmy.soundscape.screens.onboarding.PreviewWelcome
 import org.scottishtecharmy.soundscape.ui.theme.SoundscapeTheme
 
+@Preview(name = "Dansk", locale = "da", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Deutsch", locale = "de", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Ελληνικά", locale = "el", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "English", locale = "en", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Español", locale = "es", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Suomi", locale = "fi", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Français", locale = "fr", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Italiano", locale = "it", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "日本語", locale = "ja", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Norsk", locale = "nb", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Nederlands", locale = "nl", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Português (Brasil)", locale = "pt", group = "Language", showBackground = true, device = "id:small_phone")
+@Preview(name = "Svenska", locale = "sv", group = "Language", showBackground = true, device = "id:small_phone")
+annotation class LocalePreviews
+
+@Preview(name = "SmallFont", fontScale = 0.85f, group = "FontScale", device = "id:small_phone")
+@Preview(name = "LargeFont", fontScale = 1.15f, group = "FontScale", device = "id:small_phone")
+annotation class FontSizePreviews
+
+@Preview(name = "MediumPhone", group = "Devices", device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420", showSystemUi = true)
+@Preview(name = "Tablet", group = "Devices", device = Devices.PIXEL_C, showSystemUi = true)
+annotation class DevicePreviews
+
+@LocalePreviews
+@FontSizePreviews
+@DevicePreviews
+annotation class CustomPreviews
+
 class PreviewTest {
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun AudioBeaconsPreviewTest() {
         SoundscapeTheme {
@@ -20,7 +49,7 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun FinishPreviewTest() {
         SoundscapeTheme {
@@ -28,7 +57,7 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun HearingPreviewTest() {
         SoundscapeTheme {
@@ -36,7 +65,7 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun LanguagePreviewTest() {
         SoundscapeTheme {
@@ -44,7 +73,7 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun ListeningPreviewTest() {
         SoundscapeTheme {
@@ -52,7 +81,7 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun WelcomePreviewTest() {
         SoundscapeTheme {
@@ -60,12 +89,11 @@ class PreviewTest {
         }
     }
 
-    @Preview(showBackground = true)
+    @CustomPreviews
     @Composable
     fun HomePreviewTest() {
         SoundscapeTheme {
             Home()
         }
     }
-
 }
