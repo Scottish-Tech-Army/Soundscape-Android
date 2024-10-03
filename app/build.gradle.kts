2ecd692893c4f670def58fff78d68e799596be11
davecraig <davecraig@users.noreply.github.com>
2024-10-03 11:18:07 +0000

    Bump version to 0.0.42, version code 43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 42
-        versionName = "0.0.41"
+        versionCode = 43
+        versionName = "0.0.42"
 
         // Retrieve the tile provider API from local.properties. This is not under version control
         // and must be configured by each developer locally. GitHb actions fill in local.properties
