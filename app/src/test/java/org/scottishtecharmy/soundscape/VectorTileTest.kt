1b6c33734d79e72c90d29473bbb8b39e1ea8f497
davecraig <davecraig@unbalancedaudio.com>
2024-10-05 17:37:32 +0100

    Test de-duplication of POI when merging GeoJSON from vector tiles
    testVectorToGeoJson2x2 merges the GeoJSON from 4 separate vector tiles.
    Polygons and Lines simpluy overlap, but Points are cropped to within
    each tile so that on merging there are no duplicates.

--- app/src/test/java/org/scottishtecharmy/soundscape/VectorTileTest.kt
+++ app/src/test/java/org/scottishtecharmy/soundscape/VectorTileTest.kt
@@ -10,6 +10,7 @@ import org.scottishtecharmy.soundscape.geojsonparser.geojson.Point
 import org.scottishtecharmy.soundscape.geojsonparser.geojson.Polygon
 import org.scottishtecharmy.soundscape.geojsonparser.moshi.GeoJsonObjectMoshiAdapter
 import org.scottishtecharmy.soundscape.utils.getLatLonTileWithOffset
+import org.scottishtecharmy.soundscape.utils.searchFeaturesByName
 import vector_tile.VectorTile
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -26,7 +27,7 @@ class VectorTileTest {
         //  2. Parses it with the code auto-generated from the vector_tile.proto specification
         //  3. Prints it out
         val path = "src/test/res/org/scottishtecharmy/soundscape/"
-        val remoteTile = FileInputStream(path + "10212.mvt")
+        val remoteTile = FileInputStream(path + "15992x10212.mvt")
         val tile: VectorTile.Tile = VectorTile.Tile.parseFrom(remoteTile)
         println(tile.layersList)
         assert(tile.layersList.isNotEmpty())
@@ -36,6 +37,7 @@ class VectorTileTest {
         tileX: Int,
         tileY: Int,
         tileZoom: Int,
+        cropToTile: Boolean,
         geometry: MutableList<Int>
     ): List<ArrayList<LngLatAlt>> {
 
@@ -63,7 +65,7 @@ class VectorTileTest {
                         deltaX = 0
                         firstOfPair = true
                         ++lineCount
-                        if(lineCount > 1)
+                        if((lineCount > 1) && results.last().isNotEmpty())
                             results.add(arrayListOf())
                     }
 
@@ -98,15 +100,22 @@ class VectorTileTest {
 
                     // The vector tile has pixel coordinates relative to the tile origin. Convert
                     // these to global coordinates
-                    results.last().add(
-                        getLatLonTileWithOffset(
-                            tileX,
-                            tileY,
-                            tileZoom,
-                            x.toDouble() / 4096.0,
-                            y.toDouble() / 4096.0
+                    var add = true
+                    if(cropToTile) {
+                        if(x < 0 || y < 0 || x >= 4096 || y >= 4096)
+                            add = false
+                    }
+                    if(add) {
+                        results.last().add(
+                            getLatLonTileWithOffset(
+                                tileX,
+                                tileY,
+                                tileZoom,
+                                x.toDouble() / 4096.0,
+                                y.toDouble() / 4096.0
+                            )
                         )
-                    )
+                    }
                     --count
                 }
             }
@@ -127,7 +136,10 @@ class VectorTileTest {
         assert(tileOrigin2.longitude == -4.306640625)
     }
 
-    private fun vectorTileToGeoJson(tileX: Int, tileY: Int, filename: String): FeatureCollection {
+    private fun vectorTileToGeoJson(tileX: Int,
+                                    tileY: Int,
+                                    filename: String,
+                                    cropPoints: Boolean = true): FeatureCollection {
 
         val tileZoom = 15
 
@@ -158,6 +170,7 @@ class VectorTileTest {
                             tileX,
                             tileY,
                             tileZoom,
+                            false,
                             feature.geometryList)
                         for(polygon in polygons) {
                             if(polygon.first() != polygon.last())
@@ -168,9 +181,10 @@ class VectorTileTest {
 
                     VectorTile.Tile.GeomType.POINT -> {
                         val points =
-                            convertGeometry(tileX, tileY, tileZoom, feature.geometryList)
+                            convertGeometry(tileX, tileY, tileZoom, cropPoints, feature.geometryList)
                         for(point in points) {
-                            listOfGeometries.add(Point(point[0].longitude, point[0].latitude))
+                            if(point.isNotEmpty())
+                                listOfGeometries.add(Point(point[0].longitude, point[0].latitude))
                         }
                     }
 
@@ -179,6 +193,7 @@ class VectorTileTest {
                             tileX,
                             tileY,
                             tileZoom,
+                            false,
                             feature.geometryList)
                         for(line in lines) {
                             listOfGeometries.add(LineString(line))
@@ -238,7 +253,7 @@ class VectorTileTest {
 
     @Test
     fun testVectorToGeoJsonMilngavie() {
-        val geojson = vectorTileToGeoJson(15992, 10212, "10212.mvt")
+        val geojson = vectorTileToGeoJson(15992, 10212, "15992x10212.mvt")
         val adapter = GeoJsonObjectMoshiAdapter()
 
         val outputFile = FileOutputStream("milngavie.geojson")
@@ -252,6 +267,52 @@ class VectorTileTest {
         val adapter = GeoJsonObjectMoshiAdapter()
 
         val outputFile = FileOutputStream("edinburgh.geojson")
+        outputFile.write(adapter.toJson(geojson).toByteArray())
+        outputFile.close()
+    }
+
+    /** This test reads in a 2x2 array of vector tiles and merges them into a single GeoJSON.
+     * That's then saved off to a file for a visual check. There's no joining up of lines but
+     * because there are no intersections between the roads from separate tiles the GeoJSON
+     * processing code isn't really any the wiser.
+     * However, POIs have to be de-duplicated to avoid multiple all outs. The two ways to do this
+     * are:
+     *  1. Check for duplicates as we merge
+     *  2. Crop out POIs which are outside the tile during initial importing
+     * Initially going with option 2 as that's the cheapest and it's not clear why would ever want
+     * POI that are outwith the tile boundaries.
+     *
+     * When using soundscape-backend the tile array used is 3x3, but those tiles are at a higher
+     * zoom level. The vector tiles are twice the width/height and so a 2x2 array can be used with
+     * the array moving when the user location leaves the center.
+     */
+    @Test
+    fun testVectorToGeoJson2x2() {
+        val geojsonTopLeft = vectorTileToGeoJson(15991, 10212, "15991x10212.mvt")
+        val geojsonTopRight = vectorTileToGeoJson(15992, 10212, "15992x10212.mvt")
+        val geojsonBottomLeft = vectorTileToGeoJson(15991, 10213, "15991x10213.mvt")
+        val geojsonBottomRight = vectorTileToGeoJson(15992, 10213, "15992x10213.mvt")
+
+        // Merge the GeoJSON into a single FeatureCollection
+        val geojson = FeatureCollection()
+        for(feature in geojsonTopLeft.features)
+            geojson.addFeature(feature)
+        for(feature in geojsonTopRight.features)
+            geojson.addFeature(feature)
+        for(feature in geojsonBottomLeft.features)
+            geojson.addFeature(feature)
+        for(feature in geojsonBottomRight.features)
+            geojson.addFeature(feature)
+
+        val adapter = GeoJsonObjectMoshiAdapter()
+
+        // Check that the de-duplication of the points worked (without that there are two points
+        // for Graeme Pharmacy, one each from two separate tiles).
+        val searchResults = searchFeaturesByName(geojson, "Graeme")
+        println(adapter.toJson(searchResults))
+        assert(searchResults.features.size == 1)
+
+        val outputFile = FileOutputStream("2x2.geojson")
         outputFile.write(adapter.toJson(geojson).toByteArray())
         outputFile.close()
     }
