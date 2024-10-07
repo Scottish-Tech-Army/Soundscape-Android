1b230738b83af4c1d9f5e45f336a933ca80408ed
Dave Craig <davecraig@unbalancedaudio.com>
2024-10-07 16:44:00 +0100

    Switch to planetiler vector tiles for test and start intersection detection
    The vector tiles generated by planetiler use different layer names (and include
    slightly different data) from those served up by the protomaps demo map. This
    change set switches all of the test content for the VectorTilesTest code to be
    our own generated set of tiles. The only non-default options used were:
    
      --maxzoom=15 --render_maxzoom=15 --simplify-tolerance-at-max-zoom=0
    
    We're skipping a whole lot of the layers when turning into GeoJSON and only
    handling the place, poi and transportation_name layers. What this means is
    that the intersection code is actually spotting all of the intersections between
    named roads and generating a point in the GeoJSON for them. However, there are
    still other issues:
    
     1. The transportation_name layer is only for named highways/footpaths etc. All
      of the unnamed ones are in the transportation layer (I don't yet understand
      why this distinction is made!).
     2. If a line changes from a path to a road on the map then it will be described
      as an intersection. A good example of this is Hillhead Street in Milngavie at
      -4.3158,55.9421 which turns pedestrianised.
     3. I've currently just made the intersection name a concatenation of the street
      names involved. Unique ids are not making it into the GeoJSON
    
    Otherwise I'm optimistic about this working. To address item 1 either we change
    the map generated by planetiler, or we merge the transportation and
    transportation_name layers. Named roads currently appear in BOTH layers which I
    think is 'daft' so perhaps makinf the changes within planetiler would make most
    sense as it would shrink the size of the output. We may then be able to add in
    intersections there (which would increase the size of the output) possibly
    without then having to rely on the simplify-tolerance-at-max-zoom flag.

--- app/src/test/java/org/scottishtecharmy/soundscape/VectorTileTest.kt
+++ app/src/test/java/org/scottishtecharmy/soundscape/VectorTileTest.kt
@@ -27,19 +27,19 @@ class VectorTileTest {
         //  2. Parses it with the code auto-generated from the vector_tile.proto specification
         //  3. Prints it out
         val path = "src/test/res/org/scottishtecharmy/soundscape/"
-        val remoteTile = FileInputStream(path + "15992x10212.mvt")
+        val remoteTile = FileInputStream(path + "16093x10211.mvt")
         val tile: VectorTile.Tile = VectorTile.Tile.parseFrom(remoteTile)
-        println(tile.layersList)
         assert(tile.layersList.isNotEmpty())
+
+        for (layer in tile.layersList) {
+            println("Layer ${layer.name}")
+        }
     }
 
-    private fun convertGeometry(
-        tileX: Int,
-        tileY: Int,
-        tileZoom: Int,
+    private fun parseGeometry(
         cropToTile: Boolean,
         geometry: MutableList<Int>
-    ): List<ArrayList<LngLatAlt>> {
+    ): List<ArrayList<Pair<Int, Int>>> {
 
         //  Converting the geometry coordinates requires some effort. See
         //      https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md#43-geometry-encoding
@@ -49,7 +49,7 @@ class VectorTileTest {
         //
         var x = 0
         var y = 0
-        val results = mutableListOf(arrayListOf<LngLatAlt>())
+        val results = mutableListOf(arrayListOf<Pair<Int,Int>>())
         var id = 0
         var count = 0
         var deltaX = 0
@@ -106,21 +106,25 @@ class VectorTileTest {
                             add = false
                     }
                     if(add) {
-                        results.last().add(
-                            getLatLonTileWithOffset(
-                                tileX,
-                                tileY,
-                                tileZoom,
-                                x.toDouble() / 4096.0,
-                                y.toDouble() / 4096.0
-                            )
-                        )
+                        results.last().add(Pair(x,y))
                     }
                     --count
                 }
             }
         }
 
+        return results
+    }
+
+    private fun convertGeometry(tileX : Int, tileY : Int, tileZoom : Int, geometry: ArrayList<Pair<Int, Int>>) : ArrayList<LngLatAlt> {
+        val results = arrayListOf<LngLatAlt>()
+        for(point in geometry) {
+            results.add(getLatLonTileWithOffset(tileX,
+                tileY,
+                tileZoom,
+                point.first.toDouble()/4096.0,
+                point.second.toDouble()/4096.0))
+        }
         return results
     }
 
@@ -136,6 +140,58 @@ class VectorTileTest {
         assert(tileOrigin2.longitude == -4.306640625)
     }
 
+    private fun intersectionCheck(highwayPoints : HashMap< Int, ArrayList<String>>, line : ArrayList<Pair<Int, Int>>, id : String) {
+        for (point in line) {
+            if((point.first < 0) || (point.first > 4095) ||
+                (point.second < 0) || (point.second > 4095)) {
+                continue
+            }
+
+            val coordinateKey = point.first.shl(12) + point.second
+            if (highwayPoints[coordinateKey] == null) {
+                highwayPoints[coordinateKey] = arrayListOf(id)
+            }
+            else {
+                highwayPoints[coordinateKey]?.add(id)
+                //
+                // On initial testing, this intersection spotting is unreliable with the
+                // maptiler tiles :-( There's a good explanation of why here:
+                // https://gis.stackexchange.com/questions/319422/mapbox-vector-tiles-appear-to-lack-accurate-intersection-nodes
+                //
+                // There's no Roselea Drive/Strathblane Drive intersection because the
+                // Strathblane section was drawn first and then Roselea Drive joined in
+                // half way between two nodes. That node doesn't affect how you'd draw
+                // Strathblane Road and so it isn't included in its list of nodes.
+                //
+                // It's possible that we can generate the tiles so that they don't
+                // exclude intersection nodes by disabling simplification at the max
+                // zoom level, see:
+                // https://github.com/Scottish-Tech-Army/Soundscape-Android/actions/workflows/nightly.yaml
+                //
+                // This would make our tiles a little larger, but that's what you'd expect!
+                //
+                // One remaining question is whether it would then be  possible to have
+                // a single road be made up of two separate lines which would mean that
+                // we end up finding an intersection where there isn't one in the real
+                // world? Also, does that render properly on the UI map?
+                // Most roads segments wouldn't split other than at a junction, but the
+                // code has to deal with that correctly too.
+                val roads = highwayPoints[coordinateKey]
+                if (roads != null) {
+                    var intersectionNames = ""
+                    var firstRoad = true
+                    for (road in roads) {
+                        if (!firstRoad)
+                            intersectionNames += ","
+                        intersectionNames += road
+                        firstRoad = false
+                    }
+                    println("Intersection: $intersectionNames")
+                }
+            }
+        }
+    }
+
     private fun vectorTileToGeoJson(tileX: Int,
                                     tileY: Int,
                                     filename: String,
@@ -149,13 +205,24 @@ class VectorTileTest {
         val collection = FeatureCollection()
         for (layer in tile.layersList) {
             when(layer.name){
-                "landuse" -> continue
-                "earth" -> continue
-                "natural" -> continue
-                "wood" -> continue
-                "buildings" -> continue
-                "physical_line" -> continue
+                "boundary",
+                "building",
+                "housenumber",
+                "landcover",
+                "landuse",
+                "mountain_peak",
+                "park",
+                //"place",
+                //"poi",
+                "transportation",
+                //"transportation_name",
+                "water" -> {
+                    println("Skip layer: " + layer.name)
+                    continue
+                }
             }
+
+            val intersectionPoints : HashMap< Int, ArrayList<String>> = hashMapOf()
 
             println("Process layer: " + layer.name)
             for (feature in layer.featuresList) {
@@ -164,39 +231,81 @@ class VectorTileTest {
                 // same properties.
                 assert(feature.type != null)
                 val listOfGeometries = mutableListOf<GeoJsonObject>()
+
+                // Parse tags
+                var firstInPair = true
+                var key = ""
+                var value: Any? = null
+                var properties: java.util.HashMap<String, Any?>? = null
+                for (tag in feature.tagsList) {
+                    if (firstInPair)
+                        key = layer.getKeys(tag)
+                    else {
+                        val raw = layer.getValues(tag)
+                        if (raw.hasBoolValue())
+                            value = layer.getValues(tag).boolValue
+                        else if (raw.hasIntValue())
+                            value = layer.getValues(tag).intValue
+                        else if (raw.hasSintValue())
+                            value = layer.getValues(tag).sintValue
+                        else if (raw.hasFloatValue())
+                            value = layer.getValues(tag).doubleValue
+                        else if (raw.hasDoubleValue())
+                            value = layer.getValues(tag).floatValue
+                        else if (raw.hasStringValue())
+                            value = layer.getValues(tag).stringValue
+                        else if (raw.hasUintValue())
+                            value = layer.getValues(tag).uintValue
+                    }
+
+                    if (!firstInPair) {
+                        if (properties == null) {
+                            properties = HashMap()
+                        }
+                        properties.put(key, value)
+                        firstInPair = true
+                    } else
+                        firstInPair = false
+                }
+
                 when (feature.type) {
                     VectorTile.Tile.GeomType.POLYGON -> {
-                        val polygons = convertGeometry(
-                            tileX,
-                            tileY,
-                            tileZoom,
+                        val polygons = parseGeometry(
                             false,
                             feature.geometryList)
                         for(polygon in polygons) {
                             if(polygon.first() != polygon.last())
                                 polygon.add(polygon.first())
-                            listOfGeometries.add(Polygon(polygon))
+                            listOfGeometries.add(Polygon(convertGeometry(tileX, tileY, tileZoom, polygon)))
                         }
                     }
 
                     VectorTile.Tile.GeomType.POINT -> {
                         val points =
-                            convertGeometry(tileX, tileY, tileZoom, cropPoints, feature.geometryList)
+                            parseGeometry(cropPoints, feature.geometryList)
                         for(point in points) {
-                            if(point.isNotEmpty())
-                                listOfGeometries.add(Point(point[0].longitude, point[0].latitude))
+                            if(point.isNotEmpty()) {
+                                val coordinates = convertGeometry(tileX, tileY, tileZoom, point)
+                                listOfGeometries.add(
+                                    Point(coordinates[0].longitude, coordinates[0].latitude)
+                                )
+                            }
                         }
                     }
 
                     VectorTile.Tile.GeomType.LINESTRING -> {
-                        val lines = convertGeometry(
-                            tileX,
-                            tileY,
-                            tileZoom,
+                        val lines = parseGeometry(
                             false,
                             feature.geometryList)
+
+                        var name = "Unknown"
+                        properties?.let {
+                            name = properties["name"].toString()
+                        }
+
                         for(line in lines) {
-                            listOfGeometries.add(LineString(line))
+                            intersectionCheck(intersectionPoints, line, name)
+                            listOfGeometries.add(LineString(convertGeometry(tileX, tileY, tileZoom, line)))
                         }
                     }
 
@@ -209,42 +318,29 @@ class VectorTileTest {
                     // And map the tags
                     val geoFeature = Feature()
                     geoFeature.id = feature.id.toString()
-
-                    var firstInPair = true
-                    var key = ""
-                    var value: Any? = null
-                    for (tag in feature.tagsList) {
-                        if (firstInPair)
-                            key = layer.getKeys(tag)
-                        else {
-                            val raw = layer.getValues(tag)
-                            if (raw.hasBoolValue())
-                                value = layer.getValues(tag).boolValue
-                            else if (raw.hasIntValue())
-                                value = layer.getValues(tag).intValue
-                            else if (raw.hasSintValue())
-                                value = layer.getValues(tag).sintValue
-                            else if (raw.hasFloatValue())
-                                value = layer.getValues(tag).doubleValue
-                            else if (raw.hasDoubleValue())
-                                value = layer.getValues(tag).floatValue
-                            else if (raw.hasStringValue())
-                                value = layer.getValues(tag).stringValue
-                            else if (raw.hasUintValue())
-                                value = layer.getValues(tag).uintValue
-                        }
-
-                        if (!firstInPair) {
-                            if (geoFeature.properties == null) {
-                                geoFeature.properties = HashMap()
-                            }
-                            geoFeature.properties?.put(key, value)
-                            firstInPair = true
-                        } else
-                            firstInPair = false
-                    }
                     geoFeature.geometry = geometry
+                    geoFeature.properties = properties
                     collection.addFeature(geoFeature)
+                }
+            }
+            // Add points for the intersections that we found
+            for((key, intersections)  in intersectionPoints) {
+                if(intersections.size > 1) {
+                    val intersection = Feature()
+                    val x = key.shr(12)
+                    val y = key.and(0xfff)
+                    val point = arrayListOf(Pair(x, y))
+                    val coordinates = convertGeometry(tileX, tileY, tileZoom, point)
+                    intersection.geometry =
+                        Point(coordinates[0].longitude, coordinates[0].latitude)
+                    intersection.properties = HashMap()
+                    intersection.properties!!["class"] = "gd_intersection"
+                    var name = ""
+                    for (road in intersections) {
+                        name += "$road/"
+                    }
+                    intersection.properties!!["name"] = name
+                    collection.addFeature(intersection)
                 }
             }
         }
@@ -263,7 +359,7 @@ class VectorTileTest {
 
     @Test
     fun testVectorToGeoJsonEdinburgh() {
-        val geojson = vectorTileToGeoJson(16093, 10211, "10211.mvt")
+        val geojson = vectorTileToGeoJson(16093, 10211, "16093x10211.mvt")
         val adapter = GeoJsonObjectMoshiAdapter()
 
         val outputFile = FileOutputStream("edinburgh.geojson")
@@ -281,6 +377,7 @@ class VectorTileTest {
      *  2. Crop out POIs which are outside the tile during initial importing
      * Initially going with option 2 as that's the cheapest and it's not clear why would ever want
      * POI that are outwith the tile boundaries.
+     *
      *
      * When using soundscape-backend the tile array used is 3x3, but those tiles are at a higher
      * zoom level. The vector tiles are twice the width/height and so a 2x2 array can be used with
