39eca7ae7fe104ec7fac9efef70aef4ecbe4d6e9
DEMEY Fanny <fanny.pluvinage@gmail.com>
2024-10-25 10:35:25 +0100

    Fde/a11y improvement language selection (#224)
    * use dropdown instead of list
    
    * improve test for language screen
    
    * add translation and fix checkbox color in TermsScreen

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/onboarding/language/LanguageViewModel.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/onboarding/language/LanguageViewModel.kt
@@ -8,15 +8,20 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.scottishtecharmy.soundscape.audio.NativeAudioEngine
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class LanguageViewModel @Inject constructor(private val audioEngine : NativeAudioEngine): ViewModel() {
 
-    val _state : MutableStateFlow<LanguageUiState> = MutableStateFlow(LanguageUiState())
+    private val _state : MutableStateFlow<LanguageUiState> = MutableStateFlow(LanguageUiState())
     val state: StateFlow<LanguageUiState> = _state.asStateFlow()
     init {
-        _state.value = LanguageUiState(supportedLanguages = getAllLanguages(), selectedLanguageIndex = -1)
+        val allLanguages = getAllLanguages()
+        _state.value = LanguageUiState(
+            supportedLanguages = allLanguages,
+            selectedLanguageIndex = allLanguages.indexOfLanguageMatchingDeviceLanguage()
+        )
     }
     data class LanguageUiState(
         // Data for the ViewMode that affects the UI
@@ -55,6 +60,11 @@ class LanguageViewModel @Inject constructor(private val audioEngine : NativeAudi
         addIfSpeechSupports(allLanguages, Language("Svenska", "sv"))
 
         return allLanguages
+    }
+
+    private fun List<Language>.indexOfLanguageMatchingDeviceLanguage(): Int {
+        val deviceLanguage = Locale.getDefault().language
+        return this.indexOfFirst { it.code == deviceLanguage }
     }
 
     fun updateLanguage(selectedLanguage: Language): Boolean {
