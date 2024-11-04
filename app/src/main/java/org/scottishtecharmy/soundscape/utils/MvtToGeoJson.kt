a6035ec6917d0141bf4e02ce60e152ffb6d24fc3
Dave Craig <davecraig@unbalancedaudio.com>
2024-11-04 10:02:49 +0000

    Refaction intersection detection in MVT to GeoJSON translation
    The tiles from our protomaps server have been updated so that every feature
    in the transportation layer have an OSM id. This prevents un-named features
    from being merged into multilines which made it impossible to de-duplicate
    reliably. The changes here make use of this so that we now generate much
    better GeoJSON without duplicate roads (e.g. prior to this change Roselea
    Drive had 4 lines, now it has 1 with an overlap at the tile boundary).
    The explanatory comments in the code have also been extended and updated.

--- app/src/main/java/org/scottishtecharmy/soundscape/utils/MvtToGeoJson.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/utils/MvtToGeoJson.kt
@@ -18,8 +18,9 @@ private fun parseGeometry(
     //  Converting the geometry coordinates requires some effort. See
     //      https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md#43-geometry-encoding
     //
-    //  Geometries can contain multiple line segments if they have the same tags e.g. unnamed
-    //  minor roads. This is why we have to return a List of line segments.
+    //  Geometries can contain multiple line segments if they have the same tags e.g. a road with a
+    //  single name that has multiple spurs. For this reason the function returns a List of line
+    //  segments.
     //
     var x = 0
     var y = 0
@@ -107,75 +108,153 @@ data class IntersectionDetails(
     val type : String,
     val subClass : String,
     val brunnel : String,
-    val nameLayer : Boolean,
     val id : Long,
     var lineEnd : Boolean = false
 )
-private fun intersectionCheck(highwayPoints : HashMap< Int, ArrayList<IntersectionDetails>>,
-                              line : ArrayList<Pair<Int, Int>>,
-                              details : IntersectionDetails) {
-    for (point in line) {
-        if((point.first < 0) || (point.first > 4095) ||
-            (point.second < 0) || (point.second > 4095)) {
-            continue
-        }
 
-        val coordinateKey = point.first.shl(12) + point.second
-        if (highwayPoints[coordinateKey] == null) {
-            highwayPoints[coordinateKey] = arrayListOf(details)
-        }
-        else {
-            // If the road type matches but we're on a different layer then skip
-            var add = true
-            for(listDetails in highwayPoints[coordinateKey]!!) {
-                if((listDetails.nameLayer != details.nameLayer) &&
-                    (listDetails.type == details.type)) {
-                    add = false
-                    break
+class IntersectionDetection {
+
+    /**
+    * highwayPoints is a sparse map which maps from a location within the tile to a list of
+    * lines which have nodes at that point. Every node on any `transportation` line will appear in the
+    * map and if after processing all of the lines there's an intersection at that point, the map
+     * entry will have information for more than one line.
+    */
+    private val highwayNodes : HashMap< Int, ArrayList<IntersectionDetails>> = hashMapOf()
+
+    /**
+     * addLine is called for any line feature that is being added to the FeatureCollection.
+     * @param line is a new `transportation` layer line to add to the map
+     * @param details describes the line that is being added.
+     *
+     */
+    fun addLine(line : ArrayList<Pair<Int, Int>>,
+                details : IntersectionDetails) {
+        for (point in line) {
+            if((point.first < 0) || (point.first > 4095) ||
+                (point.second < 0) || (point.second > 4095)) {
+                continue
+            }
+
+            // Rather than have a 2D sparse array, turn the coordinates into a single int so that we
+            // can have a 1D sparse array instead.
+            val coordinateKey = point.first.shl(12) + point.second
+            if (highwayNodes[coordinateKey] == null) {
+                highwayNodes[coordinateKey] = arrayListOf(details)
+            }
+            else {
+                details.lineEnd = ((point == line.first()) || (point == line.last()))
+                highwayNodes[coordinateKey]?.add(details)
+
+                val roads = highwayNodes[coordinateKey]
+                if (roads != null) {
+                    var intersectionNames = ""
+                    var firstRoad = true
+                    for (road in roads) {
+                        if (!firstRoad)
+                            intersectionNames += ","
+                        intersectionNames += road.name
+                        firstRoad = false
+                    }
+                    //println("Intersection: $intersectionNames")
                 }
             }
-            if(!add) continue
-            details.lineEnd = ((point == line.first()) || (point == line.last()))
-            highwayPoints[coordinateKey]?.add(details)
-            //
-            // On initial testing, this intersection spotting is unreliable with the
-            // maptiler tiles :-( There's a good explanation of why here:
-            // https://gis.stackexchange.com/questions/319422/mapbox-vector-tiles-appear-to-lack-accurate-intersection-nodes
-            //
-            // There's no Roselea Drive/Strathblane Drive intersection because the
-            // Strathblane section was drawn first and then Roselea Drive joined in
-            // half way between two nodes. That node doesn't affect how you'd draw
-            // Strathblane Road and so it isn't included in its list of nodes.
-            //
-            // It's possible that we can generate the tiles so that they don't
-            // exclude intersection nodes by disabling simplification at the max
-            // zoom level, see:
-            // https://github.com/Scottish-Tech-Army/Soundscape-Android/actions/workflows/nightly.yaml
-            //
-            // This would make our tiles a little larger, but that's what you'd expect!
-            //
-            // One remaining question is whether it would then be  possible to have
-            // a single road be made up of two separate lines which would mean that
-            // we end up finding an intersection where there isn't one in the real
-            // world? Also, does that render properly on the UI map?
-            // Most roads segments wouldn't split other than at a junction, but the
-            // code has to deal with that correctly too.
-            val roads = highwayPoints[coordinateKey]
-            if (roads != null) {
-                var intersectionNames = ""
-                var firstRoad = true
-                for (road in roads) {
-                    if (!firstRoad)
-                        intersectionNames += ","
-                    intersectionNames += road.name
-                    firstRoad = false
+        }
+    }
+
+    /**
+     * generateIntersections goes through our hash map and adds an intersection feature to the
+     * collection wherever it finds out.
+     * @param collection is where the new intersection features are added
+     * @param tileX the tile x coordinate so that the tile relative location of the intersection can
+     * be turned into a latitude/longitude
+     * @param tileY the tile y coordinate so that the tile relative location of the intersection can
+     *      * be turned into a latitude/longitude
+     */
+    fun generateIntersections(collection: FeatureCollection, tileX : Int, tileY : Int) {
+        // Add points for the intersections that we found
+        for ((key, intersections) in highwayNodes) {
+
+            // An intersection exists where there are nodes from multiple line at the same location
+            if (intersections.size > 1) {
+
+                // Turn our coordinate key back into tile relative x,y coordinates
+                val x = key.shr(12)
+                val y = key.and(0xfff)
+                // Convert the tile relative coordinate into a LatLngAlt
+                val point = arrayListOf(Pair(x, y))
+                val coordinates = convertGeometry(tileX, tileY, ZOOM_LEVEL, point)
+
+                // Create our intersection feature to match those from soundscape-backend
+                val intersection = Feature()
+                intersection.geometry =
+                    Point(coordinates[0].longitude, coordinates[0].latitude)
+                intersection.foreign = HashMap()
+                intersection.foreign!!["feature_type"] = "highway"
+                intersection.foreign!!["feature_value"] = "gd_intersection"
+                var name = ""
+                val osmIds = arrayListOf<Long>()
+                for (road in intersections) {
+                    if(name.isNotEmpty()) {
+                        name += "/"
+                    }
+                    if (road.brunnel != "null")
+                        name += road.brunnel
+                    else if (road.subClass != "null")
+                        name += road.subClass
+                    else
+                        name += road.name
+
+                    osmIds.add(road.id)
                 }
-                //println("Intersection: $intersectionNames")
+                intersection.foreign!!["osm_ids"] = osmIds
+                intersection.properties = HashMap()
+                intersection.properties!!["name"] = name
+                collection.addFeature(intersection)
             }
         }
     }
 }
 
+/**
+ * vectorTileToGeoJson generates a GeoJSON FeatureCollection from a Mapbox Vector Tile.
+ * @param tileX is the x coordinate of the tile (the zoom level is fixed at ZOOM_LEVEL)
+ * @param tileY is the y coordinate of the tile (the zoom level is fixed at ZOOM_LEVEL)
+ * @param mvt is the Tile which has been decoded from the protobuf on its way into the application
+ * @param cropPoints is a flag to indicate whether or not crop points to be within the tile
+ *
+ * There are really two parts of this function:
+ *
+ * 1. Iterating over the features in each layers and turning their tags and geometries into GeoJSON.
+ * This is done by 'simply' following the [MVT specification](https://github.com/mapbox/vector-tile-spec/tree/master/2.1).
+ * 2. Adding some locally calculated metadata e.g. the location of intersections, and adding the
+ * ability to knit together lines that cross tile boundaries.
+ *
+ * The input tile geometries are all tile relative and using `tileX` and `tileY` we turn those into
+ * latitudes and longitudes for the GeoJSON. Although the locally calculated metadata could be done
+ * as a second pass after the initial parsing has been done, it's much more efficient to do them in
+ * a single pass. By doing that the geometries are still tile relative and much easier to handle
+ * than latitudes and longitudes.
+ *
+ * The vector tiles come from a protomaps server which is hosting a map file that we generate using
+ * `planetiler`. A stock running of `planetiler` is missing some data that we need, so we disable
+ * simplification at the maximum zoom level (which is what we're using here) and we also force the
+ * addition of a Feature id on all Features within the transportation layer. This allows us to more
+ * easily deduplicate roads and paths. This addition of the Feature id is only in our builds and not
+ * yet in upstream `planetiler`. Neither of these changes should affect the graphical rendering of
+ * the tiles which is important as we're using the tiles for that too.
+ *
+ * We currently only look at 3 layers which are defined here https://openmaptiles.org/schema/:
+ *
+ * 1. `transportation_name` which contains all of the named footways/roads etc. in the tile. We use
+ * this just to create a dictionary mapping the unique id of a road to its name.
+ * 2. `transportation` contains all footways/roads etc. including named and unnamed and so is a
+ * superset of `transportation_name`.  We use the lines from this and then lookup the names via the
+ * dictionary we made from the `transportation_name` layer with the Feature id. The result is that
+ * we should have no duplication of roads.
+ * 3. `poi` contains points of interest.
+ *
+ */
 fun vectorTileToGeoJson(tileX: Int,
                         tileY: Int,
                         mvt: VectorTile.Tile,
@@ -185,12 +264,10 @@ fun vectorTileToGeoJson(tileX: Int,
 
     val collection = FeatureCollection()
     val osmNameMap : HashMap<Long, String> = hashMapOf()
-    val intersectionPoints : HashMap< Int, ArrayList<IntersectionDetails>> = hashMapOf()
+    val intersectionDetection = IntersectionDetection()
 
-    // When processing the layers, we want to handle transportation_name before transportation.
-    // This is so that we can calculate intersections with the named lines and then discard their
-    // un-named duplicates in the transportation layer.
-    // The only other layers we want to look at is poi.
+    // When processing the layers, we want to handle transportation_name before transportation so
+    // that we can populate a dictionary of id -> names.
     for (layerId in arrayOf("transportation_name", "transportation", "poi")) {
         for(layer in mvt.layersList) {
             if(layer.name != layerId)
@@ -239,6 +316,8 @@ fun vectorTileToGeoJson(tileX: Int,
                     } else
                         firstInPair = false
                 }
+
+                // Parse geometries
                 when (feature.type) {
                     VectorTile.Tile.GeomType.POLYGON -> {
                         val polygons = parseGeometry(
@@ -280,38 +359,48 @@ fun vectorTileToGeoJson(tileX: Int,
                             feature.geometryList
                         )
 
-                        var name = ""
+                        var name : Any? = null
                         var type = ""
                         var subclass = ""
                         var brunnel = ""
                         properties?.let {
-                            name = properties["name"].toString()
-                            if(name == "null") {
+                            // Only features within the transportation_name layer should have a name
+                            name = properties["name"]
+                            if(name == null) {
                                 if(osmNameMap.containsKey(feature.id)) {
-                                    name = osmNameMap[feature.id]!!
+                                    name = osmNameMap[feature.id]
                                 } else {
+                                    // This is nameless, so use the class to describe it
                                     name = properties["class"].toString()
                                 }
                             } else {
                                 // Populate our map of ids to names
-                                osmNameMap[feature.id] = name
+                                assert(layerId == "transportation_name")
+                                if(osmNameMap.containsKey(feature.id)) {
+                                    println("Name for ${feature.id} already exists ${osmNameMap[feature.id]}")
+                                } else {
+                                    osmNameMap[feature.id] = name.toString()
+                                }
                             }
                             type = properties["class"].toString()
                             subclass = properties["subclass"].toString()
                             brunnel = properties["brunnel"].toString()
                         }
 
-                        if((layerId == "transportation_name") || true /*!osmNameMap.containsKey(feature.id)*/) {
+                        if(layerId == "transportation")
+                        {
                             for (line in lines) {
+                                if(feature.id != 0L) {
+                                    println("Feature ID is zero for ${name.toString()}")
+                                }
                                 val details = IntersectionDetails(
-                                    name,
+                                    name.toString(),
                                     type,
                                     subclass,
                                     brunnel,
-                                    layerId == "transportation_name",
                                     feature.id
                                 )
-                                intersectionCheck(intersectionPoints, line, details)
+                                intersectionDetection.addLine(line, details)
                                 listOfGeometries.add(
                                     LineString(
                                         convertGeometry(
@@ -323,7 +412,7 @@ fun vectorTileToGeoJson(tileX: Int,
                                     )
                                 )
                             }
-                        }
+                       }
                     }
 
                     VectorTile.Tile.GeomType.UNKNOWN -> {
@@ -335,6 +424,7 @@ fun vectorTileToGeoJson(tileX: Int,
                     // And map the tags
                     val geoFeature = Feature()
                     geoFeature.geometry = geometry
+                    properties!!["osm_ids"] = feature.id
                     geoFeature.properties = properties
                     geoFeature.foreign = translateProperties(properties, feature.id)
                     collection.addFeature(geoFeature)
@@ -342,53 +432,24 @@ fun vectorTileToGeoJson(tileX: Int,
             }
         }
     }
-    // Add points for the intersections that we found
-    for((key, intersections)  in intersectionPoints) {
-        if(intersections.size > 1) {
+    // Add intersections
+    intersectionDetection.generateIntersections(collection, tileX, tileY)
 
-//            // Skip any intersections where there are only two lines that are both ending, the
-//            // road type isn't changing and they are named
-//            var skip = false
-//            if(intersections.size == 2) {
-//                val road1 = intersections[0]
-//                val road2 = intersections[1]
-//                if((road1.lineEnd && road2.lineEnd) &&
-//                    (road1.type == road2.type)) {
-//                    skip = true
-//                }
-//            }
-//            //if(skip) continue
-
-            val intersection = Feature()
-            val x = key.shr(12)
-            val y = key.and(0xfff)
-            val point = arrayListOf(Pair(x, y))
-            val coordinates = convertGeometry(tileX, tileY, tileZoom, point)
-            intersection.geometry =
-                Point(coordinates[0].longitude, coordinates[0].latitude)
-            intersection.foreign = HashMap()
-            intersection.foreign!!["feature_type"] = "highway"
-            intersection.foreign!!["feature_value"] = "gd_intersection"
-            var name = ""
-            val osmIds = arrayListOf<Long>()
-            for (road in intersections) {
-                if(road.brunnel != "null")
-                    name += "${road.brunnel}/"
-                else if(road.subClass != "null")
-                    name += "${road.subClass}/"
-                else
-                    name += "${road.name}/"
-
-                osmIds.add(road.id)
-            }
-            intersection.foreign!!["osm_ids"] = osmIds
-            intersection.properties = HashMap()
-            intersection.properties!!["name"] = name
-            collection.addFeature(intersection)
-        }
-    }
     return collection
 }
+
+/**
+ * translateProperties takes the properties stored in the MVT and translates them into a set of
+ * foreign properties that nearer matches those returned by the soundscape-backend.
+ *
+ * @param properties is a map of the tags from the MVT feature
+ * @param id is the feature id which is ((OSM_ID * 10) + offset) where offset is
+ *   1 for an OSM node
+ *   2 for an OSM  way
+ *   3 for an OSM  relation
+ *
+ * @return a map of properties that can be used in the same way as those from soundscape-backend
+ */
 
 fun translateProperties(properties: HashMap<String, Any?>?, id: Long): HashMap<String, Any?> {
     val foreign : HashMap<String, Any?> = hashMapOf()
