3b6d94463b55b0a44656276f16904d5e196c78f8
Dave Craig <davecraig@unbalancedaudio.com>
2024-11-04 09:45:26 +0000

    Updat MVT test files from latest protomaps server update
    OSM ids have been added to all features in the transportation layer, so update
    the test vectors to be the latest that we're serving up.

--- app/src/test/res/org/scottishtecharmy/soundscape/update.sh
+++ app/src/test/res/org/scottishtecharmy/soundscape/update.sh
@@ -1,1 +1,5 @@
-wget http://192.168.86.39:8080/data/openmaptiles/15/15991/10212.pbf -o 15991x10212.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15991/10212.pbf -O 15991x10212.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15991/10213.pbf -O 15991x10213.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15992/10212.pbf -O 15992x10212.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15992/10213.pbf -O 15992x10213.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/16093/10211.pbf -O 16093x10211.mvt
