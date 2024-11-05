770b2f04482ac285857ca92aee3dd5e9296926c5
Dave Craig <davecraig@unbalancedaudio.com>
2024-11-05 13:23:07 +0000

    Adjust vectorTileToGeoJson to not use transportation_name layer
    We've added names to all lines that have a neam in the transportation
    layer and so we no longer need to create a dictionary from the
    transportation_name layer. We do this because that layer had merged lines
    which lost OSM ids and made de-duplication impossible. The names are only
    stored once in the MVT so it's a fairly efficient addition.
    
    I've also added some more test tiles to increase testing coverage.

--- app/src/test/res/org/scottishtecharmy/soundscape/update.sh
+++ app/src/test/res/org/scottishtecharmy/soundscape/update.sh
@@ -3,3 +3,7 @@ wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15991/10213.pbf -O 15991
 wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15992/10212.pbf -O 15992x10212.mvt
 wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15992/10213.pbf -O 15992x10213.mvt
 wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/16093/10211.pbf -O 16093x10211.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15990/10212.pbf -O 15990x10212.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15990/10213.pbf -O 15990x10213.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15990/10214.pbf -O 15990x10214.mvt
+wget https://d1wzlzgah5gfol.cloudfront.net/protomaps/15/15991/10214.pbf -O 15991x10214.mvt
