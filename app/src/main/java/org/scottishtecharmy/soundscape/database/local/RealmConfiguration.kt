474be6a3316af6468574fa0fce341e0af9b558e2
Dave Craig <davecraig@unbalancedaudio.com>
2024-11-14 16:54:46 +0000

    Remove database for tiles
    Instead of using the database as a tile cache or decoded GeoJSON, simply do
    all of the conversion work whenever the tile grid changes and keep the result
    in memory. getGridFeatureCollections returns the result instantly from memory
    and so callouts have less delay.
    The implementation doesn't try to be smart so it throws away the old grid and
    creates the FeatureCollections from scratch each time. It doesn't reuse tile
    data from the tiles that might overlap with the previous grid.
    The change works for both soundscape-backend and the protomaps backend. The
    time taken to create the grid is considerably longer for soundscape-backend
    which obviously might explain why it was originally required (~400ms vs.
    ~3 seconds for the whole grid).

--- app/src/main/java/org/scottishtecharmy/soundscape/database/local/RealmConfiguration.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/database/local/RealmConfiguration.kt
@@ -1,47 +1,15 @@
 package org.scottishtecharmy.soundscape.database.local
 
 import android.util.Log
-import org.scottishtecharmy.soundscape.database.local.model.TileData
 import io.realm.kotlin.Realm
 import io.realm.kotlin.Realm.Companion.deleteRealm
 import io.realm.kotlin.RealmConfiguration
 import org.scottishtecharmy.soundscape.database.local.model.Location
 import org.scottishtecharmy.soundscape.database.local.model.RouteData
 import org.scottishtecharmy.soundscape.database.local.model.RoutePoint
-import org.scottishtecharmy.soundscape.geoengine.SOUNDSCAPE_TILE_BACKEND
 
 object RealmConfiguration {
-    private var tileDataRealm: Realm? = null
     private var markersRealm: Realm? = null
-
-    fun getTileDataInstance(): Realm {
-        // has this object been created or opened yet?
-        if (tileDataRealm == null || tileDataRealm!!.isClosed()) {
-            // create the realm db based on the TileData model/schema
-            val config = RealmConfiguration.Builder(
-                schema = setOf(TileData::class)
-            ).name("TileData").build()
-
-            // TODO: Whilst we are working on the protomap -> GeoJSON conversion we always want to
-            //  start with a fresh tile database. Once the work is complete, then we can remove this
-            //  code.
-            if(!SOUNDSCAPE_TILE_BACKEND)
-                deleteRealm(config)
-
-            try {
-                tileDataRealm = Realm.open(config)
-            } catch(e: Exception) {
-                Log.e("Realm", "Exception opening database: $e")
-
-                // TODO: Is this really the best approach, deleting the database and trying again?
-                // We're going to delete it and try again. This is likely due to a change in schema
-                Log.e("Realm", "Deleting and re-trying")
-                deleteRealm(config)
-                tileDataRealm = Realm.open(config)
-            }
-        }
-        return tileDataRealm!!
-    }
 
     /**
      * getMarkersInstance opens the Realm database which stores the markers and routes.
