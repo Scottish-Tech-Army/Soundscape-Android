fd8ffe29b0f20dab77d63c138fd10cecf3f5e02c
Dave Craig <davecraig@unbalancedaudio.com>
2025-01-07 16:24:57 +0000

    Add FOV (field of view) searching to FeatureTree class
    Prior to this change we could get FeatureCollections by distance from
    a point, limiting the results returned to a maximum number if we desired.
    This change adds:
    
    generateNearestFeatureCollectionWithinTriangle - returns a FeatureCollection
    of the nearest features within the provided FOV triangle.
    
    generateFeatureCollectionWithinTriangle - returns a FeatureCollection
    of all the features within the provided FOV triangle.
    
    getNearestFeatureWithinTriangle - returns the nearest Feature within the
    FOV triangle.

--- app/src/main/java/org/scottishtecharmy/soundscape/geoengine/utils/FeatureTree.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/geoengine/utils/FeatureTree.kt
@@ -115,7 +115,7 @@ class FeatureTree(featureCollection: FeatureCollection?) {
         return rect
     }
 
-    private fun handleEntry(entry: Entry<Feature, Geometry?>, distance: Double, from: LngLatAlt)  : Boolean {
+    private fun entryWithinDistance(entry: Entry<Feature, Geometry?>, distance: Double, from: LngLatAlt)  : Boolean {
         when (val p = entry.geometry()) {
             is Point -> {
                 val position = LngLatAlt(p.x(), p.y())
@@ -142,7 +142,7 @@ class FeatureTree(featureCollection: FeatureCollection?) {
         return false
     }
 
-    private fun search(
+    private fun searchWithinDistance(
         lonLat: Point,
         distance: Double
     ): MutableIterable<Entry<Feature, Geometry?>>? {
@@ -153,11 +153,11 @@ class FeatureTree(featureCollection: FeatureCollection?) {
 
         return Iterables.filter(tree!!.search(bounds))
         { entry ->
-            handleEntry(entry, distance, from)
+            entryWithinDistance(entry, distance, from)
         }
     }
 
-    private fun nearest(
+    private fun nearestWithinDistance(
         lonLat: Point,
         distance: Double,
         maxCount: Int
@@ -165,7 +165,7 @@ class FeatureTree(featureCollection: FeatureCollection?) {
         val from = LngLatAlt(lonLat.x(), lonLat.y())
         return Iterables.filter(tree!!.nearest(lonLat, distance, maxCount))
         { entry ->
-            handleEntry(entry, distance, from)
+            entryWithinDistance(entry, distance, from)
         }
     }
 
@@ -196,7 +196,7 @@ class FeatureTree(featureCollection: FeatureCollection?) {
         val featureCollection = FeatureCollection()
         if(tree != null) {
             // Return only the entries within distance of our location
-            val distanceResults = Iterables.toList(search(
+            val distanceResults = Iterables.toList(searchWithinDistance(
                 Geometries.pointGeographic(location.longitude, location.latitude),
                 distance))
 
@@ -212,14 +212,14 @@ class FeatureTree(featureCollection: FeatureCollection?) {
     }
 
     /**
-     * generateNearestFeatureCollection returns a FeatureCollection containing the nearest member
-     * of the rtree that is also within distance.
+     * generateNearestFeatureCollection returns a FeatureCollection containing the nearest members
+     * of the rtree that are also within distance.
      */
 
     fun generateNearestFeatureCollection(location: LngLatAlt, distance: Double, maxCount: Int): FeatureCollection {
         val featureCollection = FeatureCollection()
         if(tree != null) {
-            val distanceResults = Iterables.toList(nearest(
+            val distanceResults = Iterables.toList(nearestWithinDistance(
                 Geometries.pointGeographic(location.longitude, location.latitude),
                 distance,
                 maxCount))
@@ -233,5 +233,200 @@ class FeatureTree(featureCollection: FeatureCollection?) {
             }
         }
         return featureCollection
+    }
+
+    /**
+     * getNearestFeature returns a Feature that is the nearest member of the rtree
+     * that is also within distance.
+     */
+    fun getNearestFeature(location: LngLatAlt, distance: Double = Double.POSITIVE_INFINITY): Feature? {
+        val distanceResults = Iterables.toList(nearestWithinDistance(
+            Geometries.pointGeographic(location.longitude, location.latitude),
+            distance,
+            1))
+
+        for (feature in distanceResults) return feature.value()
+
+        return null
+    }
+
+    /**
+     * generateFeatureCollectionWithinFov returns a FeatureCollection containing all members
+     * of the rtree that are contained within the supplied triangle
+     */
+    private fun createBoundingSquareContainingTriangle(triangle: ArrayList<LngLatAlt>) : Rectangle {
+
+        // Create a bounding rectangle that contains the triangle
+        val minLatitude = minOf(triangle[0].latitude, triangle[1].latitude, triangle[2].latitude)
+        val maxLatitude = maxOf(triangle[0].latitude, triangle[1].latitude, triangle[2].latitude)
+        val minLongitude = minOf(triangle[0].longitude, triangle[1].longitude, triangle[2].longitude)
+        val maxLongitude = maxOf(triangle[0].longitude, triangle[1].longitude, triangle[2].longitude)
+
+        return Geometries.rectangle(minLongitude,minLatitude,maxLongitude,maxLatitude)
+    }
+
+    private fun lineSegmentPassesWithinTriangle(p1: LngLatAlt, p2: LngLatAlt,
+                                                triangle: ArrayList<LngLatAlt>): Boolean {
+        // Check if the line segment intersects any of the triangle's edges
+        if (straightLinesIntersect(p1, p2, triangle[0], triangle[1]) ||
+            straightLinesIntersect(p1, p2, triangle[1], triangle[2]) ||
+            straightLinesIntersect(p1, p2, triangle[2], triangle[0])) {
+            return true
+        }
+
+        // Then check that the line segment isn't completely inside the triangle
+        val polygon = Polygon(arrayListOf(triangle[0], triangle[1], triangle[2], triangle[0]))
+        return (polygonContainsCoordinates(p1, polygon) && polygonContainsCoordinates(p2, polygon))
+    }
+
+    private fun entryWithinTriangle(entry: Entry<Feature, Geometry?>,
+                                    triangle: ArrayList<LngLatAlt>): Boolean {
+
+        when (val p = entry.geometry()) {
+            is Point -> {
+                val testPoint = LngLatAlt(p.x(), p.y())
+                // Create a closed polygon
+                val polygon = Polygon(arrayListOf(triangle[0], triangle[1], triangle[2], triangle[0]))
+                return polygonContainsCoordinates(testPoint, polygon)
+            }
+
+            is Line -> {
+                return lineSegmentPassesWithinTriangle(LngLatAlt(p.x1(), p.y1()),
+                    LngLatAlt(p.x2(), p.y2()),
+                    triangle)
+            }
+
+            is Rectangle -> {
+                // The rtree entry is a bounding box for a more complex polygon. We return true if
+                // any of the polygon coordinates are within the FOV triangle
+                val feature = entry.value()
+                for (geometry in (feature.geometry as Polygon).coordinates) {
+                    for (point in geometry) {
+                        val polygonTriangleFOV = Polygon(arrayListOf(triangle[0], triangle[1], triangle[2], triangle[0]))
+                        val containsCoordinate =
+                            polygonContainsCoordinates(point, polygonTriangleFOV)
+                        if (containsCoordinate) {
+                            return true
+                        }
+                    }
+                }
+                return false
+            }
+            else -> {
+                println("Unknown geometry type: $p")
+            }
+        }
+        return false
+    }
+
+    private fun searchWithinTriangle(
+        triangleCoordinates: ArrayList<LngLatAlt>
+    ): MutableIterable<Entry<Feature, Geometry?>>? {
+
+        // First we need to calculate an enclosing lat long rectangle for this
+        // triangle then we refine on the exact contents
+        assert(triangleCoordinates.size == 3)
+        val bounds: Rectangle = createBoundingSquareContainingTriangle(triangleCoordinates)
+
+        return Iterables.filter(tree!!.search(bounds))
+        { entry ->
+            entryWithinTriangle(entry, triangleCoordinates)
+        }
+    }
+
+    private fun nearestWithinTriangle(
+        triangleCoordinates: ArrayList<LngLatAlt>,
+        maxCount: Int
+    ): Iterable<Entry<Feature, Geometry?>>? {
+
+        // First find the features within the triangle
+        val resultsWithinTriangle = searchWithinTriangle(triangleCoordinates) ?: return null
+
+        // Check if we already have few enough results
+        if(resultsWithinTriangle.count() < maxCount) return resultsWithinTriangle
+
+        // We have too many results, so trim them down to the nearest ones
+        val nearestResults = resultsWithinTriangle.sortedBy {
+            entry ->
+            when (val p = entry.geometry()) {
+                is Point -> {
+                    val position = LngLatAlt(p.x(), p.y())
+                    triangleCoordinates[0].distance(position)
+                }
+
+                is Line -> {
+                    val thisDistance =
+                        distanceToLineString(
+                            triangleCoordinates[0], LineString(
+                                LngLatAlt(p.x1(), p.y1()),
+                                LngLatAlt(p.x2(), p.y2())
+                            )
+                        )
+                    thisDistance
+                }
+
+                is Rectangle -> {
+                    val box = BoundingBox(p.x1(), p.y1(), p.x2(), p.y2())
+                    val center = getCenterOfBoundingBox(getBoundingBoxCorners(box))
+                    triangleCoordinates[0].distance(center)
+                }
+                else -> Double.POSITIVE_INFINITY
+            }
+        }
+
+        return nearestResults.subList(
+            fromIndex = 0,
+            toIndex = maxCount
+        )
+    }
+
+    fun generateNearestFeatureCollectionWithinTriangle(location: LngLatAlt,
+                                                       left: LngLatAlt,
+                                                       right: LngLatAlt,
+                                                       maxCount: Int): FeatureCollection {
+
+        val featureCollection = FeatureCollection()
+        if(tree != null) {
+
+            val results =
+                nearestWithinTriangle(arrayListOf(location, left, right), maxCount) ?: return featureCollection
+
+            for (feature in results) {
+                featureCollection.addFeature(feature.value())
+            }
+        }
+        return featureCollection
+    }
+
+
+    fun generateFeatureCollectionWithinTriangle(location: LngLatAlt,
+                                                left: LngLatAlt,
+                                                right: LngLatAlt): FeatureCollection {
+        val featureCollection = FeatureCollection()
+        if(tree != null) {
+            val results = Iterables.toList(searchWithinTriangle(arrayListOf(location, left, right)))
+
+            val deduplicationSet = mutableSetOf<Feature>()
+            for (feature in results) {
+                if(!deduplicationSet.contains(feature.value())) {
+                    featureCollection.addFeature(feature.value())
+                    deduplicationSet.add(feature.value())
+                }
+            }
+        }
+        return featureCollection
+    }
+
+    fun getNearestFeatureWithinTriangle(location: LngLatAlt,
+                                        left: LngLatAlt,
+                                        right: LngLatAlt): Feature? {
+
+        if (tree == null)
+            return null
+
+        val results =
+            nearestWithinTriangle(arrayListOf(location, left, right), 1) ?: return null
+
+        return results.first().value()
     }
 }
