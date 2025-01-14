842effd7d7affd123bcc3ab307d960c26d3d8839
Dave Craig <davecraig@unbalancedaudio.com>
2025-01-14 10:48:07 +0000

    Add timeout to direction provider to update audio engine
    When the phone is still, or when running in the emulator, the orientation
    is not being updated as it's not changing. This was resulting in the audio
    engine not being updated which was causing its state machine to stall.
    Ideally that will be fixed inside the C++ code, but it's conceptually much
    simpler to change the calling code here. If there's no heading update within
    the timeout period of 100ms, then the audio engine is still called with the
    last heading and the current location. Note that when running on the emulator
    the location can change whilst the heading remains forever stuck at 0.0.

--- app/src/main/java/org/scottishtecharmy/soundscape/locationprovider/AndroidDirectionProvider.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/locationprovider/AndroidDirectionProvider.kt
@@ -2,12 +2,17 @@ package org.scottishtecharmy.soundscape.locationprovider
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import com.google.android.gms.location.DeviceOrientation
 import com.google.android.gms.location.DeviceOrientationListener
 import com.google.android.gms.location.DeviceOrientationRequest
 import com.google.android.gms.location.LocationServices
 import org.scottishtecharmy.soundscape.audio.NativeAudioEngine
 import java.util.concurrent.Executors
 import kotlin.math.abs
+
+const val updateTimeoutInMilliseconds = 100L
 
 class AndroidDirectionProvider(context : Context) :
     DirectionProvider() {
@@ -23,35 +28,66 @@ class AndroidDirectionProvider(context : Context) :
     }
 
     @SuppressLint("MissingPermission")
-    override fun start(audio: NativeAudioEngine, locProvider: LocationProvider){
-        listener = DeviceOrientationListener { orientation ->
-            // Always send the update to the audio engine. This immediately affects the direction
-            // and sound of the audio beacon.
-            var latitude = 0.0
-            var longitude = 0.0
-            val newHeading = orientation.headingDegrees.toDouble()
-            val location = locProvider.locationFlow.value
-            if(location != null) {
-                latitude = location.latitude
-                longitude = location.longitude
-            }
-            audio.updateGeometry(
-                latitude,
-                longitude,
-                newHeading
-            )
-            // Now send the value to all of the other interested parties. These do not require
-            // such regular updates, or sub-degree precision, so only send changes in  heading
-            // greater than 1 degree and throttle to 50Hz maximum.
-            val delta = newHeading - lastUpdateValue
-            if(abs(delta) > 1.0) {
-                val now = System.currentTimeMillis()
-                if ((now - lastUpdateTime) > 20) {
-                    mutableOrientationFlow.value =
-                        orientation  // Emit the DeviceOrientation object
+    override fun start(audio: NativeAudioEngine, locProvider: LocationProvider) {
 
-                    lastUpdateTime = now
-                    lastUpdateValue = newHeading
+        // We need to have a regular timeout so that when the device orientation isn't changing the
+        // audio engine is still updated. This ensures that the location is updated and also that
+        // the audio engine queue is processed when there's no movement at all.
+        listener = object : DeviceOrientationListener {
+            val handler = Handler(Looper.getMainLooper())
+
+            var timeoutRunnable: Runnable = object : Runnable {
+                override fun run() {
+                    // Update the audio engine with the current location and the last heading that
+                    // we processed.
+                    val location = locProvider.locationFlow.value
+                    audio.updateGeometry(
+                        location?.latitude ?: 0.0,
+                        location?.longitude ?: 0.0,
+                        lastUpdateValue
+                    )
+                    // Re-schedule timeout
+                    handler.postDelayed(this, updateTimeoutInMilliseconds)
+                }
+            }
+
+            init {
+                // Start the timeout
+                handler.postDelayed(timeoutRunnable, updateTimeoutInMilliseconds)
+            }
+
+            override fun onDeviceOrientationChanged(orientation: DeviceOrientation) {
+
+                val newHeading = orientation.headingDegrees.toDouble()
+
+                // If the heading has changed by more than 1 degree and hasn't changed in the last
+                // 20ms, update all interested parties.
+                val delta = newHeading - lastUpdateValue
+                if (abs(delta) > 1.0) {
+                    val now = System.currentTimeMillis()
+                    if ((now - lastUpdateTime) > 20) {
+
+                        // Cancel timeout as we have processed an update
+                        handler.removeCallbacks(timeoutRunnable)
+
+                        // Send the update to the audio engine. This affects the direction and sound
+                        // of the audio beacon.
+                        val location = locProvider.locationFlow.value
+                        audio.updateGeometry(
+                            location?.latitude ?: 0.0,
+                            location?.longitude ?: 0.0,
+                            newHeading
+                        )
+
+                        // Emit the DeviceOrientation object
+                        mutableOrientationFlow.value = orientation
+
+                        lastUpdateTime = now
+                        lastUpdateValue = newHeading
+
+                        // Re-schedule timeout
+                        handler.postDelayed(timeoutRunnable, updateTimeoutInMilliseconds)
+                    }
                 }
             }
         }
@@ -59,8 +95,7 @@ class AndroidDirectionProvider(context : Context) :
         // OUTPUT_PERIOD_DEFAULT = 50Hz / 20ms
         val request = DeviceOrientationRequest.Builder(DeviceOrientationRequest.OUTPUT_PERIOD_DEFAULT)
             .build()
-        // Thought I could use a Looper here like for location but it seems to want an Executor instead
-        // Not clear on what the difference is...
+
         val executor = Executors.newSingleThreadExecutor()
         fusedOrientationProviderClient.requestOrientationUpdates(request, executor, listener)
     }
