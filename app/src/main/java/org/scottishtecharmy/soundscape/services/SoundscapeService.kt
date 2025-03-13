Scottish-Tech-Army/Soundscape-Android

commit 7b13f2cbc90831e3e2a149043100e5cd7a4bb4eb

Fix race in StreetPreview startup
The StreetPreview state was being set after the GeoEngine was started. This
meant there was a race between the state being set and the call to
tileGridUpdated which used the state. A simple fix, plus adjust the UI
buttons to be more sensibly sized.

app/src/main/java/org/scottishtecharmy/soundscape/services/SoundscapeService.kt
@@ -137,15 +137,18 @@ class SoundscapeService : MediaSessionService() {
             locationProvider = AndroidLocationProvider(this)
             directionProvider = AndroidDirectionProvider(this)
         }
+
+        // Set the StreetPreview state prior to starting the location provider. Otherwise there's a
+        // race in the tileGridUpdated callback.
+        _streetPreviewFlow.value = StreetPreviewState(if(on) StreetPreviewEnabled.INITIALIZING else StreetPreviewEnabled.OFF)
+
         locationProvider.start(this)
         directionProvider.start(audioEngine, locationProvider)
         geoEngine.start(application, locationProvider, directionProvider, this)
-
-        _streetPreviewFlow.value = StreetPreviewState(if(on) StreetPreviewEnabled.INITIALIZING else StreetPreviewEnabled.OFF)
     }
 
     fun tileGridUpdated() {
-        if(_streetPreviewFlow.value.enabled == StreetPreviewEnabled.INITIALIZING) {
+        if(_streetPreviewFlow.value.enabled != StreetPreviewEnabled.OFF) {
             _streetPreviewFlow.value = StreetPreviewState(
                 StreetPreviewEnabled.ON,
                 geoEngine.streetPreviewGo()
@@ -481,7 +484,7 @@ class SoundscapeService : MediaSessionService() {
      * It indicates that the user has selected the direction of travel in which they which to move.
      */
     fun streetPreviewGo() {
-        _streetPreviewFlow.value = StreetPreviewState(StreetPreviewEnabled.ON, geoEngine.streetPreviewGo())
+        _streetPreviewFlow.value = _streetPreviewFlow.value.copy(choices = geoEngine.streetPreviewGo())
     }
 
     fun appInForeground(foreground: Boolean) {

app/src/main/java/org/scottishtecharmy/soundscape/screens/home/home/StreetPreviewUi.kt
@@ -1,5 +1,6 @@
 package org.scottishtecharmy.soundscape.screens.home.home
 
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import org.scottishtecharmy.soundscape.geoengine.StreetPreviewState
 import org.scottishtecharmy.soundscape.geoengine.utils.calculateHeadingOffset
 import org.scottishtecharmy.soundscape.screens.home.StreetPreviewFunctions
 import org.scottishtecharmy.soundscape.ui.theme.mediumPadding
+import org.scottishtecharmy.soundscape.ui.theme.spacing
 
 @Composable
 fun StreetPreview(
@@ -66,7 +68,8 @@ fun StreetPreview(
             onClick = {
                 streetPreviewFunctions.go()
             },
-            text = text
+            text = text,
+            modifier = Modifier.defaultMinSize(minHeight = spacing.targetSize * 3)
         )
     }
     NavigationButton(
@@ -74,6 +77,7 @@ fun StreetPreview(
             streetPreviewFunctions.exit()
         },
         // TODO: Internationalization
-        text = stringResource(R.string.street_preview_exit)
+        text = stringResource(R.string.street_preview_exit),
+        horizontalPadding = spacing.large
     )
 }

commit 00e12b0dc385d26ef644f0312cdb5fb983a0b650

Fix break to StreetPreview in commit 7b13f2cbc90831e3e2a149043100e5cd7a4bb4eb
Accidentally made StreetPreview automatically jump forwards...oops.

app/src/main/java/org/scottishtecharmy/soundscape/services/SoundscapeService.kt
@@ -133,7 +133,7 @@ class SoundscapeService : MediaSessionService() {
     }
 
     fun tileGridUpdated() {
-        if(_streetPreviewFlow.value.enabled != StreetPreviewEnabled.OFF) {
+        if(_streetPreviewFlow.value.enabled == StreetPreviewEnabled.INITIALIZING) {
             _streetPreviewFlow.value = StreetPreviewState(
                 StreetPreviewEnabled.ON,
                 geoEngine.streetPreviewGo()