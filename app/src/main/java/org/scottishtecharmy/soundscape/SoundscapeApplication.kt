842eccee7374210a1ea6063937ff236c8a2f6443
Dave Craig <davecraig@unbalancedaudio.com>
2025-03-19 11:01:17 +0000

    Revert "Auto-generate markdown document from Help strings in app"
    This reverts commit b1e0f7d0aea65a9b895e65cdda92e9efea4826ca.
    
    Adding robolectric seemed to break much of the syntax checking in Android
    Studio :-(

--- app/src/main/java/org/scottishtecharmy/soundscape/SoundscapeApplication.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/SoundscapeApplication.kt
@@ -1,7 +1,6 @@
 package org.scottishtecharmy.soundscape
 
 import android.app.Application
-import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,9 +10,7 @@ class SoundscapeApplication : Application(){
     }
     companion object {
         init {
-            if(!Build.FINGERPRINT.contains("robolectric")) {
-                System.loadLibrary(BuildConfig.FMOD_LIB)
-            }
+            System.loadLibrary(BuildConfig.FMOD_LIB)
         }
     }
 }
