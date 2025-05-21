c2c4fe2e411f8270e55ab3d15c7350bea04df35b
davecraig <8530624+davecraig@users.noreply.github.com>
2025-05-21 09:45:02 +0000

    Bump version to 0.0.76, version code 77

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 76
-        versionName = "0.0.75"
+        versionCode = 77
+        versionName = "0.0.76"
 
 //  We don't currently require a Tile provider API key
 //
