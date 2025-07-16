b9717c18b34b9737aaa73a5f70a6cfa4f6b245ce
davecraig <8530624+davecraig@users.noreply.github.com>
2025-07-16 17:51:17 +0000

    Bump version to 0.0.89, version code 90

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 89
-        versionName = "0.0.88"
+        versionCode = 90
+        versionName = "0.0.89"
 
         // Retrieve the tile provider URL and API key from local.properties. This is not under
         // version control and must be configured by each developer locally. GitHub actions fill in
