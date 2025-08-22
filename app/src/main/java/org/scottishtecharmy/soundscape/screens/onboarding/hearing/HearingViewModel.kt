1e784742f8ea5834bbe702feaae831abdc47ac4a
Dave Craig <davecraig@unbalancedaudio.com>
2025-08-22 16:25:25 +0100

    updateGeometry is now called automatically on AudioEngine
    Previously, if there was no service then updateGeometry was never called.
    Now it is, so we don't need this extra call in the onboarding screen.

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/onboarding/hearing/HearingViewModel.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/onboarding/hearing/HearingViewModel.kt
@@ -16,18 +16,7 @@ class HearingViewModel @Inject constructor(private val audioEngine : NativeAudio
 
     fun playSpeech(speechText: String) {
         // Set our listener position, and play the speech
-        // TODO: If updateGeometry isn't called, then the audioEngine doesn't move on to   the next
-        //  queued text to speech. That resulted in the Listen button only working one time.
-        //  Calling updateGeometry (which in the service is called every 30ms) sorts this out.
-        //  We should consider another way of doing this.
         audioEngine.clearTextToSpeechQueue()
-        audioEngine.updateGeometry(
-            listenerLatitude = 0.0,
-            listenerLongitude = 0.0,
-            listenerHeading = 0.0,
-            focusGained = true,
-            duckingAllowed = false
-        )
         audioEngine.createTextToSpeech(speechText, AudioType.LOCALIZED)
     }
 
