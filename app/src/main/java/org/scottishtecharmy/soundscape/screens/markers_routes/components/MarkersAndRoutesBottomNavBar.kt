c80df69f01d7b90ef7dd105ea467e350074ce624
Dave Craig <davecraig@unbalancedaudio.com>
2025-08-23 19:10:49 +0100

    Ensure that we show translations of Markers and Routes
    The strings were hard coded instead of localized.

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/components/MarkersAndRoutesBottomNavBar.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/components/MarkersAndRoutesBottomNavBar.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.scottishtecharmy.soundscape.screens.markers_routes.navigation.ScreensForMarkersAndRoutes
@@ -50,7 +51,7 @@ fun MarkersAndRoutesTabs(
                 onClick = { setRoutesAndMarkersTab(item == ScreensForMarkersAndRoutes.Routes) },
                 label = {
                     Text(
-                        item.title,
+                        stringResource(item.title),
                         style = MaterialTheme.typography.bodyLarge,
                         color = MaterialTheme.colorScheme.onSurface
                     )
--- app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/navigation/ScreensForMarkersAndRoutes.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/navigation/ScreensForMarkersAndRoutes.kt
@@ -1,20 +1,21 @@
 package org.scottishtecharmy.soundscape.screens.markers_routes.navigation
 
+import androidx.compose.ui.res.stringResource
 import org.scottishtecharmy.soundscape.R
 
 sealed class ScreensForMarkersAndRoutes(
     val route: String,
-    val title: String,
+    val title: Int,
     val iconResId: Int? = null,
 ) {
     data object Markers : ScreensForMarkersAndRoutes(
         route = "markers",
-        title = "Markers",
+        title = R.string.markers_title,
         iconResId = R.drawable.ic_markers,
     )
     data object Routes : ScreensForMarkersAndRoutes(
         route = "routes",
-        title = "Routes",
+        title = R.string.routes_title,
         iconResId = R.drawable.ic_routes,
     )
 }
