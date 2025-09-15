b7db9ace8ce0769f0c81dde0b2a0f4223dae2896
Dave Craig <davecraig@unbalancedaudio.com>
2025-09-15 12:41:45 +0100

    Add example unit test code for generating offline maps hierarchy
    testHierarchyGenerationFromManifest illustrates how the manifest GeoJSON can
    be used to generate a continent/country/region hierarchy to present to a UI.
    The leaves are the GeoJSON Features which contain all of the information about
    each extract.

--- app/src/test/java/org/scottishtecharmy/soundscape/OfflineMapsTest.kt
+++ app/src/test/java/org/scottishtecharmy/soundscape/OfflineMapsTest.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import org.scottishtecharmy.soundscape.geoengine.utils.FeatureTree
 import org.scottishtecharmy.soundscape.geoengine.utils.getDistanceToFeature
 import org.scottishtecharmy.soundscape.geoengine.utils.rulers.CheapRuler
+import org.scottishtecharmy.soundscape.geojsonparser.geojson.Feature
 import org.scottishtecharmy.soundscape.geojsonparser.geojson.FeatureCollection
 import org.scottishtecharmy.soundscape.geojsonparser.geojson.LngLatAlt
 import org.scottishtecharmy.soundscape.geojsonparser.moshi.GeoJsonObjectMoshiAdapter
@@ -47,7 +48,7 @@ class OfflineMapsTest {
      * This test demonstrates loading in the extract GeoJSON and using it to find nearby extracts
      */
     @Test
-    fun loadExtractMetadata() {
+    fun testManifestInFeatureTree() {
         val path = "src/test/res/org/scottishtecharmy/soundscape/"
         val metadataFile = FileInputStream(path + "manifest.geojson").bufferedReader().use { it.readText() }
 
@@ -62,5 +63,79 @@ class OfflineMapsTest {
         checkLocation( LngLatAlt(-0.1277653, 51.5074456), tree) // London
         checkLocation( LngLatAlt(-13.6872370, 57.5962764), tree) // Rockall!
         checkLocation( LngLatAlt(106.7021468, 10.7756293), tree)
+    }
+
+    /**
+     * This test demonstrates the organising of the manifest by continent and country
+     */
+    @Test
+    fun testHierarchyGenerationFromManifest() {
+        val path = "src/test/res/org/scottishtecharmy/soundscape/"
+        val metadataFile =
+            FileInputStream(path + "manifest.geojson").bufferedReader().use { it.readText() }
+
+        // Load in the metadata GeoJSON file
+        val adapter = GeoJsonObjectMoshiAdapter()
+        val collection = adapter.fromJson(metadataFile) as FeatureCollection
+
+        // This code just makes a map of continents to countries to provinces
+
+        val continents: MutableMap<String, MutableMap<String, Any>> = mutableMapOf()
+        for (feature in collection) {
+            val continent = feature.properties?.get("continent")
+            continent?. let { continent ->
+
+                // Add any continents that we haven't previously seen
+                if (!continents.contains(continent)) {
+                    continents[continent as String] = mutableMapOf()
+                }
+
+                if (feature.properties?.get("feature_type") == "country") {
+                    // Add new country
+                    val name = feature.properties?.get("name") as String
+                    val countriesMap = continents[continent]!!
+                    countriesMap[name] = feature
+                }
+                else {
+                    // This Feature is not a country, but is a province of some kind
+                    val countryName = feature.properties?.get("country_name") as String
+                    val countriesMap = continents[continent]!!
+
+                    if (!countriesMap.contains(countryName)) {
+                        countriesMap[countryName] = mutableMapOf<String, MutableMap<String, Feature>>()
+                    }
+                    val regionMap = countriesMap[countryName] as MutableMap<String, Feature>
+                    val name = feature.properties?.get("name")
+                    if (name == null)
+                        println("Bug with unusual Russian region")
+                    else
+                        regionMap[name as String] = feature
+                }
+            }
+        }
+
+        // Dump out the hierarchy
+        for (continent in continents) {
+            println("${continent.key} ->")
+            for(country in continent.value) {
+                val states = country.value
+                if(states is MutableMap<*, *>) {
+                    println("\t${country.key} ->")
+                    for (state in states) {
+                        val feature = state.value as Feature
+                        val extractSize = feature.properties?.get("extract-size") as Double
+                        val formattedSize = String.format("%.2fMB", extractSize / 1024 / 1024)
+                        val extractName = feature.properties?.get("filename")
+                        println("\t\t${state.key}, $extractName $formattedSize")
+                    }
+                } else {
+                    val feature = country.value as Feature
+                    val extractSize = feature.properties?.get("extract-size") as Double
+                    val formattedSize = String.format("%.2fMB", extractSize / 1024 / 1024)
+                    val extractName = feature.properties?.get("filename")
+                    println("\t${country.key}, $extractName $formattedSize")
+                }
+            }
+        }
     }
 }
