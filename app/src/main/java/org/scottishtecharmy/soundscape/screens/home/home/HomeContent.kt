3c3d010dc5f0ff2ed0ae0f34dd54397e2f2d469c
Dave Craig <davecraig@unbalancedaudio.com>
2025-10-24 13:19:54 +0100

    Refactor route playback UI card to be more accessible
    There's a more obvious difference between route and audio beacon playback as
    the previous/next buttons don't appear for simple beacon playback. A stop button
    is now available for routes and the Talkback descriptions and hints are much
    improved.

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/home/home/HomeContent.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/home/home/HomeContent.kt
@@ -3,6 +3,7 @@ package org.scottishtecharmy.soundscape.screens.home.home
 import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -29,15 +30,18 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.maplibre.android.maps.MapLibreMap.OnMapLongClickListener
 import org.scottishtecharmy.soundscape.R
 import org.scottishtecharmy.soundscape.components.NavigationButton
@@ -52,6 +56,7 @@ import org.scottishtecharmy.soundscape.screens.home.RouteFunctions
 import org.scottishtecharmy.soundscape.screens.home.StreetPreviewFunctions
 import org.scottishtecharmy.soundscape.screens.home.data.LocationDescription
 import org.scottishtecharmy.soundscape.screens.home.locationDetails.generateLocationDetailsRoute
+import org.scottishtecharmy.soundscape.screens.talkbackDescription
 import org.scottishtecharmy.soundscape.screens.talkbackHint
 import org.scottishtecharmy.soundscape.services.BeaconState
 import org.scottishtecharmy.soundscape.services.RoutePlayerState
@@ -61,6 +66,69 @@ import org.scottishtecharmy.soundscape.ui.theme.mediumPadding
 import org.scottishtecharmy.soundscape.ui.theme.smallPadding
 import org.scottishtecharmy.soundscape.ui.theme.spacing
 
+data class ButtonState(
+    val onClick: () -> Unit,
+    val imageVector: ImageVector,
+    val contentDescriptionId: Int,
+    val hintActive: Int = 0,
+    val hintInactive: Int = 0,
+)
+@Composable
+fun CardStatefulButton(states: Array<ButtonState>,
+                       currentState: Int,
+                       active: Boolean = true,
+                       testTag: String) {
+
+    val state = remember(currentState) { states[currentState] }
+    var modifier = Modifier
+        .talkbackDescription(stringResource(state.contentDescriptionId))
+        .testTag(testTag)
+
+    if(active) {
+        if (state.hintActive != 0)
+            modifier = modifier
+                .talkbackHint(stringResource(state.hintActive))
+                .clearAndSetSemantics {  } // The built in description is laborious
+    } else
+        modifier = modifier
+            .clearAndSetSemantics {  } // The built in description is laborious
+
+    Button(
+        onClick = state.onClick,
+        shape = MaterialTheme.shapes.small,
+        colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
+        contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
+        modifier = modifier
+    )
+    {
+        Icon(
+            modifier = modifier,
+            imageVector = state.imageVector,
+            tint = if (active)
+                MaterialTheme.colorScheme.onSurface
+            else
+                MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f),
+            contentDescription =
+                stringResource(state.contentDescriptionId)
+        )
+    }
+}
+
+@Composable
+fun CardButton(onClick: () -> Unit,
+               imageVector: ImageVector,
+               active: Boolean = true,
+               contentDescriptionId: Int,
+               hintActive: Int = 0,
+               hintInactive: Int = 0,
+               testTag: String) {
+    CardStatefulButton(
+        arrayOf(ButtonState(onClick, imageVector, contentDescriptionId, hintActive, hintInactive)),
+        0,
+        active,
+        testTag
+    )
+}
 @Composable
 fun HomeContent(
     location: LngLatAlt?,
@@ -137,10 +205,14 @@ fun HomeContent(
                     Card(modifier = Modifier
                         .smallPadding(),
                     ) {
-                        Column(verticalArrangement = Arrangement.spacedBy(spacing.small)) {
                             Row {
                                 Text(
-                                    text = "${routePlayerState.routeData.route.name} - ${routePlayerState.currentWaypoint + 1}/${routePlayerState.routeData.markers.size}",
+                                    text = stringResource(
+                                        R.string.route_waypoint_progress).format(
+                                            routePlayerState.routeData.route.name,
+                                            routePlayerState.currentWaypoint + 1,
+                                            routePlayerState.routeData.markers.size
+                                        ),
                                     style = MaterialTheme.typography.labelLarge,
                                     modifier = Modifier.smallPadding()
                                 )
@@ -167,105 +239,56 @@ fun HomeContent(
                                 horizontalArrangement = Arrangement.SpaceEvenly,
                                 verticalAlignment = androidx.compose.ui.Alignment.Bottom
                             ) {
-                                Button(
-                                    onClick = { routeFunctions.skipPrevious() },
-                                    colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
-                                    modifier = Modifier.testTag("routeSkipPrevious")
-                                )
-                                {
-                                    Icon(
-                                        modifier = Modifier.talkbackHint(
-                                            stringResource(R.string.route_detail_action_previous_hint)
-                                        ),
+                                if(!routePlayerState.beaconOnly) {
+                                    CardButton(
+                                        onClick = { routeFunctions.skipPrevious() },
                                         imageVector = Icons.Filled.SkipPrevious,
-                                        tint = if (routePlayerState.currentWaypoint == 0)
-                                                   MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
-                                               else
-                                                   MaterialTheme.colorScheme.onSurface,
-                                        contentDescription = stringResource(R.string.route_detail_action_previous)
+                                        active = (routePlayerState.currentWaypoint != 0),
+                                        contentDescriptionId = R.string.route_detail_action_previous,
+                                        hintActive = R.string.route_detail_action_previous_hint,
+                                        hintInactive = R.string.route_detail_action_previous_disabled_hint,
+                                        testTag = "routeSkipPrevious"
                                     )
-                                }
-                                Button(
-                                    onClick = { routeFunctions.skipNext() },
-                                    colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
-                                    modifier = Modifier.testTag("routeSkipNext")
-                                )
-                                {
-                                    Icon(
-                                        modifier = Modifier.talkbackHint(
-                                            stringResource(R.string.route_detail_action_next_hint)
-                                        ),
+                                    CardButton(
+                                        onClick = { routeFunctions.skipNext() },
                                         imageVector = Icons.Filled.SkipNext,
-                                        tint = if (routePlayerState.currentWaypoint < routePlayerState.routeData.markers.size - 1)
-                                                   MaterialTheme.colorScheme.onSurface
-                                               else
-                                                   MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f),
-                                        contentDescription = stringResource(R.string.route_detail_action_next)
+                                        active = (routePlayerState.currentWaypoint < routePlayerState.routeData.markers.size - 1),
+                                        contentDescriptionId = R.string.route_detail_action_next,
+                                        hintActive = R.string.route_detail_action_next_hint,
+                                        hintInactive = R.string.route_detail_action_next_disabled_hint,
+                                        testTag = "routeSkipNext"
+                                    )
+                                    CardButton(
+                                        onClick = { onNavigate("${HomeRoutes.RouteDetails.route}/${routePlayerState.routeData.route.routeId}") },
+                                        imageVector = Icons.Filled.Info,
+                                        contentDescriptionId = R.string.behavior_experiences_route_nav_title,
+                                        testTag = "routeDetails"
                                     )
                                 }
-                                Button(
-                                    onClick = {
-                                        if (routePlayerState.beaconOnly) {
-                                            routeFunctions.stop()
-                                        } else {
-                                            onNavigate("${HomeRoutes.RouteDetails.route}/${routePlayerState.routeData.route.routeId}")
-                                        }
-                                    },
-                                    colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
-                                    modifier = Modifier.testTag("routeStopOrDetails")
+                                CardStatefulButton(
+                                    arrayOf(
+                                        ButtonState({ routeFunctions.mute() }, Icons.AutoMirrored.Filled.VolumeOff, R.string.beacon_action_unmute_beacon, R.string.beacon_action_unmute_beacon_acc_hint),
+                                        ButtonState({ routeFunctions.mute() }, Icons.AutoMirrored.Filled.VolumeMute, R.string.beacon_action_mute_beacon, R.string.beacon_action_mute_beacon_acc_hint),
+                                    ),
+                                    currentState = if (beaconState?.muteState == true) 0 else 1,
+                                    testTag = "RouteMute"
                                 )
-                                {
-                                    if (routePlayerState.beaconOnly) {
-                                        Icon(
-                                            modifier = Modifier,
-                                            imageVector = Icons.Filled.Stop,
-                                            contentDescription = stringResource(R.string.route_detail_action_stop_route),
-                                        )
-                                    } else {
-                                        Icon(
-                                            modifier = Modifier,
-                                            imageVector = Icons.Filled.Info,
-                                            contentDescription = stringResource(R.string.behavior_experiences_route_nav_title),
-                                        )
-                                    }
-                                }
-                                Button(
-                                    onClick = { routeFunctions.mute() },
-                                    colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
-                                    modifier = Modifier.testTag("routeMute")
+
+                                CardButton(
+                                    onClick = { routeFunctions.stop() },
+                                    imageVector = Icons.Filled.Stop,
+                                    contentDescriptionId = R.string.route_detail_action_stop_route,
+                                    testTag = "routeStop"
                                 )
-                                {
-                                    Icon(
-                                        modifier = Modifier.talkbackHint(
-                                            if (beaconState?.muteState == true)
-                                                stringResource(R.string.beacon_action_unmute_beacon_acc_hint)
-                                            else
-                                                stringResource(R.string.beacon_action_mute_beacon_acc_hint)
-                                        ),
-                                        imageVector = if (beaconState?.muteState == true)
-                                            Icons.AutoMirrored.Filled.VolumeOff
-                                        else
-                                            Icons.AutoMirrored.Filled.VolumeMute,
-                                        contentDescription = if (beaconState?.muteState == true)
-                                            stringResource(R.string.beacon_action_unmute_beacon)
-                                        else
-                                            stringResource(R.string.beacon_action_mute_beacon),
-                                    )
-                                }
+
                                 if(showMap) {
-                                    Button(
+                                    CardButton(
                                         onClick = { fullscreenMap.value = !fullscreenMap.value },
-                                        colors = if (!LocalInspectionMode.current) currentAppButtonColors else ButtonDefaults.buttonColors(),
-                                        modifier = Modifier.testTag("routeFullScreenMap")
+                                        imageVector = Icons.Rounded.Fullscreen,
+                                        contentDescriptionId = R.string.location_detail_full_screen_hint,
+                                        testTag = "routeFullScreenMap"
                                     )
-                                    {
-                                        Icon(
-                                            imageVector = Icons.Rounded.Fullscreen,
-                                            contentDescription = stringResource(R.string.location_detail_full_screen_hint)
-                                        )
-                                    }
                                 }
-                            }
                         }
                     }
                 } else {
@@ -342,7 +365,7 @@ fun PreviewHomeContent() {
     val routePlayerState = RoutePlayerState(
         routeData = RouteWithMarkers(
             RouteEntity(
-                name = "Route 1",
+                name = "Test",
                 description = "Description 1"
             ),
             listOf(
@@ -360,6 +383,49 @@ fun PreviewHomeContent() {
                 )
             )
         ),
+        currentWaypoint = 0
+    )
+
+    HomeContent(
+        location = LngLatAlt(),
+        beaconState = null,
+        routePlayerState = routePlayerState,
+        heading = 45.0f,
+        onNavigate = {},
+        onMapLongClick = { false },
+        searchBar = {},
+        streetPreviewState = StreetPreviewState(StreetPreviewEnabled.OFF),
+        streetPreviewFunctions = StreetPreviewFunctions(null),
+        routeFunctions = RouteFunctions(null),
+        getCurrentLocationDescription = { LocationDescription("Current location", LngLatAlt()) },
+        goToAppSettings = {},
+        fullscreenMap = remember { mutableStateOf(false) },
+        permissionsRequired = false,
+        showMap = true
+    )
+}
+
+@Preview
+@Composable
+fun PreviewHomeContentBeacon() {
+    val routePlayerState = RoutePlayerState(
+
+        routeData = RouteWithMarkers(
+            RouteEntity(
+                routeId = 0,
+                name = "Milngavie Town Hall",
+                description = ""
+            ),
+            listOf(
+                MarkerEntity(
+                    name = "Marker 1",
+                    longitude = -74.0060,
+                    latitude = .7128,
+                    fullAddress = "Description 1"
+                )
+            )
+        ),
+        beaconOnly = true,
         currentWaypoint = 0
     )
 
