f528151de7ffc2a129963147e340601bd5861a4c
Dave Craig <davecraig@unbalancedaudio.com>
2025-10-24 10:10:08 +0100

    Improve Talkback for CustomTextField (marker and route names and descriptions)
    The CustomTextField is now based on OutlinedTextField and configures the hint and
    ContentDescription more usefully.
    Also removed some English only strings that I found in the CustomFloatingActionButton.

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/components/CustomTextField.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/markers_routes/components/CustomTextField.kt
@@ -1,30 +1,41 @@
 package org.scottishtecharmy.soundscape.screens.markers_routes.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextField
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.scottishtecharmy.soundscape.R
+import org.scottishtecharmy.soundscape.screens.talkbackDescription
+import org.scottishtecharmy.soundscape.screens.talkbackHint
+import org.scottishtecharmy.soundscape.ui.theme.smallPadding
 import org.scottishtecharmy.soundscape.ui.theme.spacing
 
 @Composable
 fun CustomTextField(
+    fieldName: String,
+    fieldHint: String,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
@@ -32,12 +43,20 @@ fun CustomTextField(
     shape: Shape = RoundedCornerShape(spacing.extraSmall),
     isSingleLine: Boolean = true  // Optional single-line behavior
 ) {
-    TextField(
+    OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
         textStyle = textStyle,
         shape = shape,
         singleLine = isSingleLine,
+        label = {
+            Text(
+                modifier = Modifier
+                    .padding(top = spacing.small, bottom = spacing.small),
+                text = fieldName,
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        },
         trailingIcon = {
             if (value.isNotEmpty()) {
                 IconButton(
@@ -46,7 +65,7 @@ fun CustomTextField(
                 ) {
                     Icon(
                         Icons.Filled.Clear,
-                        contentDescription = "Clear Text",
+                        contentDescription = stringResource(R.string.text_field_clear_text),
                         tint = MaterialTheme.colorScheme.onSurface
                     )
                 }
@@ -57,21 +76,40 @@ fun CustomTextField(
             capitalization = KeyboardCapitalization.Sentences
         ),
         modifier = modifier
+            .talkbackDescription("$fieldName...$value")
+            .talkbackHint(fieldHint)
+            .clearAndSetSemantics {  } // The built in description is laborious
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CustomTextFieldPreview() {
-    var textValue by remember { mutableStateOf("Hello") }
-
-    CustomTextField(
-        value = textValue,
-        onValueChange = { textValue = it },
-        textStyle = TextStyle(fontSize = 18.sp),
-        shape = RoundedCornerShape(spacing.extraSmall),
-        isSingleLine = true
-    )
+    Column(
+        modifier = Modifier
+            .smallPadding()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+        CustomTextField(
+            fieldName = "Name",
+            fieldHint = "Name of the marker",
+            value = "The bench by the market",
+            onValueChange = { },
+            textStyle = TextStyle(fontSize = 18.sp),
+            shape = RoundedCornerShape(spacing.extraSmall),
+            isSingleLine = true
+        )
+        HorizontalDivider(thickness = 50.dp)
+        CustomTextField(
+            fieldName = "Description",
+            fieldHint = "Description of marker",
+            value = "",
+            onValueChange = { },
+            textStyle = TextStyle(fontSize = 18.sp),
+            shape = RoundedCornerShape(spacing.extraSmall),
+            isSingleLine = true
+        )
+    }
 }
 
 
