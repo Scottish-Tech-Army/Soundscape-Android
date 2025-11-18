58c90979dc4a358e7acabcf38c117df2ea5c1cdf
davecraig <8530624+davecraig@users.noreply.github.com>
2025-11-18 16:53:25 +0000

    Bump version to 0.0.128, version code 129

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 128
-        versionName = "0.0.127"
+        versionCode = 129
+        versionName = "0.0.128"
 
         // Maintaining this list means that we can exclude translations that aren't complete yet
         resourceConfigurations.addAll(listOf(
