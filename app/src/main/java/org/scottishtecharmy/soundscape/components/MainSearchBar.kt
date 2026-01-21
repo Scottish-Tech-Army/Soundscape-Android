52a183db416f5fdeac25d9569d4f3a844b91a623
Dave Craig <davecraig@unbalancedaudio.com>
2026-01-21 10:18:24 +0000

    Adjust Search UI behaviour to be more consistent
    It's now possible to click on search results and return to the search list
    without losing the search text from the input field. Unfortunately it's not
    so easy to have the cursor in the input field appear at the end of the text
    but it's better than before.

--- app/src/main/java/org/scottishtecharmy/soundscape/components/MainSearchBar.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/components/MainSearchBar.kt
@@ -1,6 +1,5 @@
 package org.scottishtecharmy.soundscape.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,31 +14,31 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.CollectionInfo
 import androidx.compose.ui.semantics.CollectionItemInfo
 import androidx.compose.ui.semantics.collectionInfo
 import androidx.compose.ui.semantics.collectionItemInfo
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import org.scottishtecharmy.soundscape.R
 import org.scottishtecharmy.soundscape.geojsonparser.geojson.LngLatAlt
 import org.scottishtecharmy.soundscape.screens.home.SearchFunctions
 import org.scottishtecharmy.soundscape.screens.home.data.LocationDescription
-import org.scottishtecharmy.soundscape.screens.markers_routes.components.CustomTextField
-import org.scottishtecharmy.soundscape.ui.theme.smallPadding
 import org.scottishtecharmy.soundscape.ui.theme.spacing
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,6 +51,26 @@ fun MainSearchBar(
     onItemClick: (LocationDescription) -> Unit,
     userLocation: LngLatAlt?
 ) {
+    val focusRequester = remember { FocusRequester() }
+    var textFieldValue by remember {
+        mutableStateOf(TextFieldValue(searchText, TextRange(searchText.length)))
+    }
+    LaunchedEffect(searchText) {
+        if (searchText != textFieldValue.text) {
+            textFieldValue = textFieldValue.copy(
+                text = searchText,
+                selection = TextRange(searchText.length)
+            )
+        }
+    }
+    LaunchedEffect(isSearching) {
+        if (isSearching) {
+            focusRequester.requestFocus()
+            // Force cursor to the end when returning/expanding
+            textFieldValue = textFieldValue.copy(selection = TextRange(searchText.length))
+        }
+    }
+
     SearchBar(
         modifier =
             Modifier
@@ -66,12 +85,24 @@ fun MainSearchBar(
         shape = RoundedCornerShape(spacing.small),
         inputField = {
             SearchBarDefaults.InputField(
-                query = searchText,
-                onQueryChange = { searchFunctions.onSearchTextChange(it) },
+                // TODO: SearchBarDefaults doesn't currently take a TextFieldValue which means that
+                //  we can't control the cursor position within it. Rather than write a new search
+                //  bar we'll await this being fixed in Material3.
+                query = textFieldValue.text,
+                onQueryChange = { newText ->
+                    println("onQueryChange $newText ${newText.length} ${textFieldValue.selection}")
+                    textFieldValue = textFieldValue.copy(text = newText, selection = TextRange(newText.length))
+                    searchFunctions.onSearchTextChange(newText)
+                },
                 onSearch = { searchFunctions.onTriggerSearch() },
                 expanded = isSearching,
-                onExpandedChange = { searchFunctions.onToggleSearch() },
+                onExpandedChange = { expanded ->
+                    if (expanded != isSearching) {
+                        searchFunctions.onToggleSearch()
+                    }
+                },
                 placeholder = { Text(stringResource(id = R.string.search_choose_destination)) },
+                modifier = Modifier.focusRequester(focusRequester),
                 leadingIcon = {
                     when {
                         !isSearching -> {
@@ -83,7 +114,10 @@ fun MainSearchBar(
 
                         else -> {
                             IconButton(
-                                onClick = { searchFunctions.onToggleSearch() },
+                                onClick = {
+                                    searchFunctions.onToggleSearch()
+                                    searchFunctions.onSearchTextChange("")
+                                },
                             ) {
                                 Icon(
                                     Icons.AutoMirrored.Rounded.ArrowBack,
@@ -97,7 +131,11 @@ fun MainSearchBar(
             )
         },
         expanded = isSearching,
-        onExpandedChange = { searchFunctions.onToggleSearch() },
+        onExpandedChange = { expanded ->
+            if (expanded != isSearching) {
+                searchFunctions.onToggleSearch()
+            }
+        }
     ) {
         Column(
             modifier =
@@ -108,7 +146,8 @@ fun MainSearchBar(
                                 rowCount = itemList.size, // Total number of items
                                 columnCount = 1, // Single-column list
                             )
-                    }.fillMaxSize()
+                    }
+                    .fillMaxSize()
         ) {
             LazyColumn(modifier = Modifier.padding(top = spacing.medium)) {
                 itemsIndexed(itemList) { index, item ->
@@ -122,7 +161,6 @@ fun MainSearchBar(
                                     true,
                                     {
                                         onItemClick(item)
-                                        searchFunctions.onToggleSearch()
                                     }
                                 )
                             ),
