cb9b8df10816dc11290b0671682481033718bdf2
Dave Craig <davecraig@unbalancedaudio.com>
2026-01-30 13:27:17 +0000

    Add hints to PlacesNearby folders and centralise them
    The folders were duplicated, so centralise them and add hint to better
    describe their actions.

--- app/src/main/java/org/scottishtecharmy/soundscape/screens/home/placesnearby/PlacesNearbyList.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/screens/home/placesnearby/PlacesNearbyList.kt
@@ -27,13 +27,24 @@ import org.scottishtecharmy.soundscape.components.FolderItem
 import org.scottishtecharmy.soundscape.components.LocationItem
 import org.scottishtecharmy.soundscape.components.LocationItemDecoration
 import org.scottishtecharmy.soundscape.screens.home.locationDetails.generateLocationDetailsRoute
+import org.scottishtecharmy.soundscape.screens.talkbackHint
 import org.scottishtecharmy.soundscape.ui.theme.spacing
 import org.scottishtecharmy.soundscape.utils.process
 
 data class Folder(
-    val name: String,
+    val nameResource: Int,
     val icon: ImageVector,
-    val filter: String
+    val filter: String,
+    val talkbackDescriptionResource: Int
+)
+
+val placesNearbyFolders = listOf(
+    Folder(R.string.filter_all, Icons.Rounded.ControlCamera, "", R.string.all_places_nearby_description),
+    Folder(R.string.filter_transit, Icons.Rounded.DirectionsBus, "transit", R.string.public_transit_places_nearby_description),
+    Folder(R.string.filter_food_drink, Icons.Rounded.Fastfood, "food_and_drink", R.string.food_drink_places_nearby_description),
+    Folder(R.string.filter_groceries, Icons.Rounded.LocalGroceryStore, "groceries", R.string.groceries_places_nearby_description),
+    Folder(R.string.filter_banks, Icons.Rounded.AttachMoney, "banks", R.string.banks_places_nearby_description),
+    Folder(R.string.osm_intersection, Icons.Rounded.ForkLeft, "intersections", R.string.intersections_places_nearby_description),
 )
 
 @Composable
@@ -43,14 +54,6 @@ fun PlacesNearbyList(
     onClickFolder: (String, String) -> Unit,
     modifier: Modifier
 ) {
-    val folders = listOf(
-        Folder(stringResource(R.string.filter_all), Icons.Rounded.ControlCamera, ""),
-        Folder(stringResource(R.string.filter_transit), Icons.Rounded.DirectionsBus, "transit"),
-        Folder(stringResource(R.string.filter_food_drink), Icons.Rounded.Fastfood, "food_and_drink"),
-        Folder(stringResource(R.string.filter_groceries), Icons.Rounded.LocalGroceryStore, "groceries"),
-        Folder(stringResource(R.string.filter_banks), Icons.Rounded.AttachMoney, "banks"),
-        Folder(stringResource(R.string.osm_intersection), Icons.Rounded.ForkLeft, "intersections"),
-    )
     val context = LocalContext.current
     val locations = remember(uiState) {
         filterLocations(uiState, context)
@@ -61,20 +64,23 @@ fun PlacesNearbyList(
         verticalArrangement = Arrangement.spacedBy(spacing.tiny),
     ) {
         if(uiState.level  == 0) {
-            itemsIndexed(folders) { index, folderItem ->
+            itemsIndexed(placesNearbyFolders) { index, folderItem ->
                 if(index == 0) {
                     HorizontalDivider(
                         thickness = spacing.tiny,
                         color = MaterialTheme.colorScheme.outlineVariant
                     )
                 }
+                val name = stringResource(folderItem.nameResource)
                 FolderItem(
-                    name = folderItem.name,
+                    name = name,
                     icon = folderItem.icon,
                     onClick = {
-                        onClickFolder(folderItem.filter, folderItem.name)
+                        onClickFolder(folderItem.filter, name)
                     },
-                    modifier = Modifier.testTag("placesNearby-$index")
+                    modifier = Modifier
+                        .testTag("placesNearby-$index")
+                        .talkbackHint(stringResource(folderItem.talkbackDescriptionResource))
                 )
             }
         } else {
