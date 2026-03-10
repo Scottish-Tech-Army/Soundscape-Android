bcf39df008688d6fb249862f6d986e720c33d49b
davecraig <8530624+davecraig@users.noreply.github.com>
2026-03-10 16:34:52 +0000

    Bump version to 0.4.3, version code 177

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 176
-        versionName = "0.4.2"
+        versionCode = 177
+        versionName = "0.4.3"
 
         // Maintaining this list means that we can exclude translations that aren't complete yet
         resourceConfigurations.addAll(listOf(
