e5168d2bf22dd5bd3506765a62e55e31496620ee
Dave Craig <davecraig@unbalancedaudio.com>
2026-04-15 11:54:30 +0100

    Bump release version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 188
-        versionName = "0.4.14"
+        versionName = "1.0.0"
 
         // Maintaining this list means that we can exclude translations that aren't complete yet
         resourceConfigurations.addAll(listOf(
