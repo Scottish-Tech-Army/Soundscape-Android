74debbf71e146c80b14a74f684b3018252d3b02a
Dave Craig <davecraig@unbalancedaudio.com>
2026-04-17 12:55:12 +0100

    Move TileDecompress to using okio and in commonMain

---
+++ shared/src/commonMain/kotlin/org/scottishtecharmy/soundscape/geoengine/utils/TileDecompress.kt
@@ -0,0 +1,32 @@
+package org.scottishtecharmy.soundscape.geoengine.utils
+
+import okio.Buffer
+import okio.GzipSource
+import okio.buffer
+import vector_tile.Tile
+
+fun decompressGzip(compressedData: ByteArray): ByteArray? {
+    return try {
+        val source = Buffer().write(compressedData)
+        GzipSource(source).buffer().readByteArray()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
+
+fun decompressTile(compressionType: Byte?, rawTileData: ByteArray): Tile? {
+    when (compressionType) {
+        1.toByte() -> {
+            return Tile.ADAPTER.decode(rawTileData)
+        }
+
+        2.toByte() -> {
+            val decompressedTile = decompressGzip(rawTileData)
+            return decompressedTile?.let { Tile.ADAPTER.decode(it) }
+        }
+
+        else -> assert(false)
+    }
+    return null
+}
