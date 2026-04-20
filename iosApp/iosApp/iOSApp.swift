69af8e8e43e045dd37008da8d42b6ad35bc26cfc
David Craig <davidcraig@davids-mac-mini.lan>
2026-04-20 10:10:22 +0100

    Fix screen size issue

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ComposeView()
+                .ignoresSafeArea(.all)
         }
     }
+}
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
