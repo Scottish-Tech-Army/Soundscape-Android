98c92d7174959330f8ca06c9d6d6733a9bda211c
luke <luke.william.duncan@gmail.com>
2026-07-02 15:18:03 +0100

    - Added dialog to appear when it is determined that the user has not seen this dialog before. - Options presented to user to start audio tour are yes/no - When dismissed, enum ordinal is committed to sharedpreferences and, unless cleared, should never be shown again. - I have decided to use the following states to indicate how the user interacted the first time for metric or debug purposes: - 0: Incomplete - Initial value - 1: Dismissed - When user selects "No" or dismisses dialog - 2: Completed - When user selects "Yes".
    Disabled by Dave for now - just need to check interaction with new release dialog and established users.

--- app/src/main/java/org/scottishtecharmy/soundscape/viewmodels/home/HomeState.kt
+++ app/src/main/java/org/scottishtecharmy/soundscape/viewmodels/home/HomeState.kt
@@ -17,6 +17,17 @@ data class HomeState(
     var searchItems: List<LocationDescription>? = null,
     var routesTabSelected: Boolean = true,
     var currentRouteData: RoutePlayerState = RoutePlayerState(),
-    var permissionsRequired: Boolean = false,
     var voiceCommandListening: Boolean = false,
+    // I've disabled auto tutorial startup until I've checked how it behaves with users who have
+    // been using the app a long time and also the new release dialog.
+    val initialTutorialState: InitialTutorialState = InitialTutorialState.DISMISSED,
 )
+
+fun HomeState.shouldShowInitialTutorialDialog() =
+    initialTutorialState == InitialTutorialState.INCOMPLETE
+
+enum class InitialTutorialState {
+    INCOMPLETE,
+    DISMISSED,
+    COMPLETED,
+}
