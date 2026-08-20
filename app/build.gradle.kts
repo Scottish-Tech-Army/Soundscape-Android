8c20e441de0285115bf1195db10907e159243cf8
davecraig <8530624+davecraig@users.noreply.github.com>
2026-08-20 13:26:26 +0000

    Bump version to 2.0.32, version code 1032

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1031
-        versionName = "2.0.31"
+        versionCode = 1032
+        versionName = "2.0.32"
 
 
         // Retrieve the tile provider URL and API key from local.properties. This is not under
